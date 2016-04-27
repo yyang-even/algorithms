be4b08fb415b17e481875a5d3ab99b80bccb9bce
Yang Yang <yyang@newfieldwireless.com>
2016-04-27 13:44:51 -0700

    Framework: Add unit test framework
    Change-Id: I877e9d8b48b2b7afc4c037e009f1612c10a7779c

--- recurrent/the_tower_of_hanoi.cpp
+++ recurrent/the_tower_of_hanoi.cpp
@@ -9,22 +9,25 @@ typedef unsigned InputType;
  * The objective is to transfer the entire tower to one of the other pegs, moving only one disk
  * at a time and never moving a larger one onto a smaller.
  */
-unsigned long HanoiTower_recursive(const InputType n) {
+unsigned long HanoiTowerRecursive(const InputType n) {
     if (n == 0) {
         return 0;
     } else {
-        return (HanoiTower_recursive(n - 1) << 1) + 1;
+        return (HanoiTowerRecursive(n - 1) << 1) + 1;
     }
 }
 
-unsigned long HanoiTower_closedform(const InputType n) {
+unsigned long HanoiTowerClosedform(const InputType n) {
     return ((unsigned long)1 << n) - 1;
 }
 
-const InputType LOWER_BOUND = 0;
-constexpr InputType UPPER_BOUND = LONG_BITS_NUM - 1;
+const InputType LOWER = 0;
+constexpr InputType UPPER = LONG_BITS_NUM;
 const InputType SAMPLE = 8;
-std::uniform_int_distribution<InputType> distribution(LOWER_BOUND, UPPER_BOUND);
+std::uniform_int_distribution<InputType> distribution(LOWER, UPPER);
 
-BENCHMARK_SUIT(HanoiTower_recursive);
-BENCHMARK_SUIT(HanoiTower_closedform);
+BENCHMARK_SUIT(HanoiTowerRecursive);
+BENCHMARK_SUIT(HanoiTowerClosedform);
+
+SIMPLE_TEST_SUIT(HanoiTowerRecursive, 0, ULONG_MAX, 255);
+SIMPLE_TEST_SUIT(HanoiTowerClosedform, 0, ULONG_MAX, 255);
