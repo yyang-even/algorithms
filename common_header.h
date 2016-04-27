c668d068be328f9442c56797e31cbbc4c73375f9
Yang Yang <yyang@newfieldwireless.com>
2016-04-27 13:44:51 -0700

    Framework: Add unit test framework
    1. Add unit test framework
    2. Add unit test for the_tower_of_hanoi and lines_in_the_plane
    3. Remove underscores from some identifiers, since gtest doesn't
        support underscore in test names.
    
    Change-Id: I877e9d8b48b2b7afc4c037e009f1612c10a7779c

--- common_header.h
+++ common_header.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <climits>
 #include <random>
 #include <string>
 
 #include "3rdParty/nonius.h++"
+#ifdef WANT_TESTS
+#include "gtest/gtest.h"
+#endif
 
 #define SIMPLE_BENCHMARK(func_name, input) namespace {    \
     NONIUS_BENCHMARK((std::string(#func_name) + "(" + #input + ")"), []() {    \
@@ -20,11 +24,27 @@ std::mt19937 generator(rd());
     })  \
 }
 
-#define BENCHMARK_SUIT(func_name) namespace {    \
-    SIMPLE_BENCHMARK(func_name, LOWER_BOUND);    \
-    SIMPLE_BENCHMARK(func_name, UPPER_BOUND);    \
-    SIMPLE_BENCHMARK(func_name, SAMPLE);         \
-    RAMDOM_BENCHMARK(func_name);                 \
+#define BENCHMARK_SUIT(func_name) namespace {   \
+    SIMPLE_BENCHMARK(func_name, LOWER);         \
+    SIMPLE_BENCHMARK(func_name, UPPER);         \
+    SIMPLE_BENCHMARK(func_name, SAMPLE);        \
+    RAMDOM_BENCHMARK(func_name);                \
 }
 
-constexpr unsigned LONG_BITS_NUM = (sizeof(unsigned long) << 2);
+constexpr unsigned LONG_BITS_NUM = (sizeof(unsigned long) * CHAR_BIT);
+
+#ifdef WANT_TESTS
+#define SIMPLE_TEST(func_name, input, expectedValue) namespace {    \
+    TEST(func_name##Test, Test##input) {                            \
+        EXPECT_EQ(expectedValue, func_name(input));                 \
+    }                                                               \
+}
+#else
+#define SIMPLE_TEST(func_name, input, expectedValue) namespace {}
+#endif
+
+#define SIMPLE_TEST_SUIT(func_name, lowerExpect, upperExpect, sampleExpect) namespace { \
+    SIMPLE_TEST(func_name, LOWER, lowerExpect);                                         \
+    SIMPLE_TEST(func_name, UPPER, upperExpect);                                         \
+    SIMPLE_TEST(func_name, SAMPLE, sampleExpect);                                       \
+}
