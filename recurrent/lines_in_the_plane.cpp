94baf7aa2d6092f02a62b43732beefc8ce1043a2
Yang Yang <yyang@newfieldwireless.com>
2016-04-27 13:44:51 -0700

    Framework: Add unit test framework
    1. Add unit test framework
    2. Add unit test for the_tower_of_hanoi and lines_in_the_plane
    3. Remove underscores from some identifiers, since gtest doesn't
        support underscore in test names.
    
    Change-Id: I877e9d8b48b2b7afc4c037e009f1612c10a7779c

--- recurrent/lines_in_the_plane.cpp
+++ recurrent/lines_in_the_plane.cpp
@@ -10,15 +10,15 @@ typedef unsigned InputType;
  * How many slices of pizza can a person obtain by making n straight cuts with a pizza knife?
  * What is the maximum number of regions defined by n lines in the plane?
  */
-unsigned long PlaneLines_recursive(const InputType n) {
+unsigned long PlaneLinesRecursive(const InputType n) {
     if (n == 0) {
         return 1;
     } else {
-        return PlaneLines_recursive(n - 1) + n;
+        return PlaneLinesRecursive(n - 1) + n;
     }
 }
 
-unsigned long PlaneLines_closedform(const InputType n) {
+unsigned long PlaneLinesClosedform(const InputType n) {
     return n * (n + 1) / 2 + 1;
 }
 
@@ -29,15 +29,19 @@ unsigned long PlaneLines_closedform(const InputType n) {
  * Suppose that instead of straight lines we use bent lines, each containing one "zig."
  * What is the maximum number of regions determined by n such bent lines in the plane?
  */
-unsigned long PlaneBentLines_closedform(const InputType n) {
+unsigned long PlaneBentLinesClosedform(const InputType n) {
     return ((n * n) << 1) - n + 1;
 }
 
-const InputType LOWER_BOUND = 0;
-constexpr InputType UPPER_BOUND = std::sqrt(static_cast<double>((~InputType(0)) / 2));
+const InputType LOWER = 0;
+constexpr InputType UPPER = std::sqrt(static_cast<double>((~InputType(0)) / 2));
 const InputType SAMPLE = 14;
-std::uniform_int_distribution<InputType> distribution(LOWER_BOUND, UPPER_BOUND);
+std::uniform_int_distribution<InputType> distribution(LOWER, UPPER);
 
-BENCHMARK_SUIT(PlaneLines_recursive);
-BENCHMARK_SUIT(PlaneLines_closedform);
-BENCHMARK_SUIT(PlaneBentLines_closedform);
+BENCHMARK_SUIT(PlaneLinesRecursive);
+BENCHMARK_SUIT(PlaneLinesClosedform);
+BENCHMARK_SUIT(PlaneBentLinesClosedform);
+
+SIMPLE_TEST_SUIT(PlaneLinesRecursive, 1, UPPER, 105);
+SIMPLE_TEST_SUIT(PlaneLinesClosedform, 1, UPPER, 105);
+SIMPLE_TEST_SUIT(PlaneBentLinesClosedform, 1, UPPER, 105);
