2961b1722d0f464723905c746fbe4ae9812e814b
Yang Yang <yyang.even@gmail.com>
2016-06-22 23:01:18 -0700

    Recurrent: Add Josephus3 and JosephusK
    Change framework to support multi-parameter functions.
    Add JosephusProblem3().
    Add JosephusK.
    
    Change-Id: Ic413fefd76041272a0ece2da8e8e67365a55935f

--- recurrent/the_tower_of_hanoi.cpp
+++ recurrent/the_tower_of_hanoi.cpp
@@ -109,15 +109,15 @@ SIMPLE_BENCHMARK(HanoiTowerClosedform, UPPER);
 SIMPLE_BENCHMARK(HanoiTowerClosedform, SAMPLE);
 RANDOM_BENCHMARK(HanoiTowerClosedform, LOWER, UPPER);
 
-SIMPLE_TEST(HanoiTowerRecursive, LOWER, 0);
-SIMPLE_TEST(HanoiTowerRecursive, UPPER, ULONG_MAX / 2);
-SIMPLE_TEST(HanoiTowerRecursive, SAMPLE, 255);
+SIMPLE_TEST(HanoiTowerRecursive, TestLOWER, 0, LOWER);
+SIMPLE_TEST(HanoiTowerRecursive, TestUPPER, ULONG_MAX / 2, UPPER);
+SIMPLE_TEST(HanoiTowerRecursive, TestSAMPLE, 255, SAMPLE);
 
-SIMPLE_TEST(HanoiTowerClosedform, LOWER, 0);
-SIMPLE_TEST(HanoiTowerClosedform, UPPER, ULONG_MAX / 2);
-SIMPLE_TEST(HanoiTowerClosedform, SAMPLE, 255);
+SIMPLE_TEST(HanoiTowerClosedform, TestLOWER, 0, LOWER);
+SIMPLE_TEST(HanoiTowerClosedform, TestUPPER, ULONG_MAX / 2, UPPER);
+SIMPLE_TEST(HanoiTowerClosedform, TestSAMPLE, 255, SAMPLE);
 
-MUTUAL_TEST(HanoiTowerRecursive, HanoiTowerClosedform, LOWER, UPPER);
+MUTUAL_RANDOM_TEST(HanoiTowerRecursive, HanoiTowerClosedform, LOWER, UPPER);
 
 
 constexpr InputType IndirectHanoiTowerUPPER = LONG_BITS_NUM / 3;
@@ -132,16 +132,18 @@ SIMPLE_BENCHMARK(IndirectHanoiTowerClosedform, IndirectHanoiTowerUPPER);
 SIMPLE_BENCHMARK(IndirectHanoiTowerClosedform, SAMPLE);
 RANDOM_BENCHMARK(IndirectHanoiTowerClosedform, LOWER, IndirectHanoiTowerUPPER);
 
-SIMPLE_TEST(IndirectHanoiTowerRecursive, LOWER, 0);
-SIMPLE_TEST(IndirectHanoiTowerRecursive, IndirectHanoiTowerUPPER, 10460353202);
-SIMPLE_TEST(IndirectHanoiTowerRecursive, SAMPLE, 6560);
-
-SIMPLE_TEST(IndirectHanoiTowerClosedform, LOWER, 0);
-SIMPLE_TEST(IndirectHanoiTowerClosedform, IndirectHanoiTowerUPPER, 10460353202);
-SIMPLE_TEST(IndirectHanoiTowerClosedform, SAMPLE, 6560);
-
-MUTUAL_TEST(IndirectHanoiTowerRecursive, IndirectHanoiTowerClosedform, LOWER,
+SIMPLE_TEST(IndirectHanoiTowerRecursive, TestLOWER, 0, LOWER);
+SIMPLE_TEST(IndirectHanoiTowerRecursive, TestIndirectHanoiTowerUPPER, 10460353202,
             IndirectHanoiTowerUPPER);
+SIMPLE_TEST(IndirectHanoiTowerRecursive, TestSAMPLE, 6560, SAMPLE);
+
+SIMPLE_TEST(IndirectHanoiTowerClosedform, TestLOWER, 0, LOWER);
+SIMPLE_TEST(IndirectHanoiTowerClosedform, TestIndirectHanoiTowerUPPER, 10460353202,
+            IndirectHanoiTowerUPPER);
+SIMPLE_TEST(IndirectHanoiTowerClosedform, TestSAMPLE, 6560, SAMPLE);
+
+MUTUAL_RANDOM_TEST(IndirectHanoiTowerRecursive, IndirectHanoiTowerClosedform, LOWER,
+                   IndirectHanoiTowerUPPER);
 
 const InputType DoubleHanoiTowerUPPER = UPPER - 2;
 
@@ -162,16 +164,20 @@ SIMPLE_BENCHMARK(DoubleHanoiTowerBClosedform, DoubleHanoiTowerUPPER);
 SIMPLE_BENCHMARK(DoubleHanoiTowerBClosedform, SAMPLE);
 RANDOM_BENCHMARK(DoubleHanoiTowerBClosedform, LOWER, DoubleHanoiTowerUPPER);
 
-SIMPLE_TEST(DoubleHanoiTowerARecursive, LOWER, 0);
-SIMPLE_TEST(DoubleHanoiTowerARecursive, DoubleHanoiTowerUPPER, 4611686018427387902);
-SIMPLE_TEST(DoubleHanoiTowerARecursive, SAMPLE, 510);
+SIMPLE_TEST(DoubleHanoiTowerARecursive, TestLOWER, 0, LOWER);
+SIMPLE_TEST(DoubleHanoiTowerARecursive, TestDoubleHanoiTowerUPPER, 4611686018427387902,
+            DoubleHanoiTowerUPPER);
+SIMPLE_TEST(DoubleHanoiTowerARecursive, TestSAMPLE, 510, SAMPLE);
 
-SIMPLE_TEST(DoubleHanoiTowerAClosedform, LOWER, 0);
-SIMPLE_TEST(DoubleHanoiTowerAClosedform, DoubleHanoiTowerUPPER, 4611686018427387902);
-SIMPLE_TEST(DoubleHanoiTowerAClosedform, SAMPLE, 510);
+SIMPLE_TEST(DoubleHanoiTowerAClosedform, TestLOWER, 0, LOWER);
+SIMPLE_TEST(DoubleHanoiTowerAClosedform, TestDoubleHanoiTowerUPPER, 4611686018427387902,
+            DoubleHanoiTowerUPPER);
+SIMPLE_TEST(DoubleHanoiTowerAClosedform, TestSAMPLE, 510, SAMPLE);
 
-MUTUAL_TEST(DoubleHanoiTowerARecursive, DoubleHanoiTowerAClosedform, LOWER, DoubleHanoiTowerUPPER);
+MUTUAL_RANDOM_TEST(DoubleHanoiTowerARecursive, DoubleHanoiTowerAClosedform, LOWER,
+                   DoubleHanoiTowerUPPER);
 
-SIMPLE_TEST(DoubleHanoiTowerBClosedform, DoubleHanoiTowerBLOWER, 3);
-SIMPLE_TEST(DoubleHanoiTowerBClosedform, DoubleHanoiTowerUPPER, 9223372036854775803);
-SIMPLE_TEST(DoubleHanoiTowerBClosedform, SAMPLE, 1019);
+SIMPLE_TEST(DoubleHanoiTowerBClosedform, TestDoubleHanoiTowerBLOWER, 3, DoubleHanoiTowerBLOWER);
+SIMPLE_TEST(DoubleHanoiTowerBClosedform, TestDoubleHanoiTowerUPPER, 9223372036854775803,
+            DoubleHanoiTowerUPPER);
+SIMPLE_TEST(DoubleHanoiTowerBClosedform, TestSAMPLE, 1019, SAMPLE);
