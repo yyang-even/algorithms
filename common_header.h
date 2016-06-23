8973b4a7b3fe134ab86942c76e2bed1d84da54d5
Yang Yang <yyang.even@gmail.com>
2016-06-22 23:01:18 -0700

    Recurrent: Add Josephus3 and JosephusK
    Change framework to support multi-parameter functions.
    Add JosephusProblem3().
    Add JosephusK.
    
    Change-Id: Ic413fefd76041272a0ece2da8e8e67365a55935f

--- common_header.h
+++ common_header.h
@@ -11,43 +11,43 @@
 #include "gtest/gtest.h"
 #endif
 
-#define SIMPLE_BENCHMARK(func_name, input) namespace {    \
-    NONIUS_BENCHMARK((std::string(#func_name) + "(" + #input + ")"), []() {    \
-        return func_name(input);   \
-    })  \
+#define SIMPLE_BENCHMARK(func_name, inputs...) namespace {                      \
+    NONIUS_BENCHMARK((std::string(#func_name) + "(" + #inputs + ")"), []() {    \
+        return func_name(inputs);                                               \
+    })                                                                          \
 }
 
 std::random_device rd;
 std::mt19937 generator(rd());
-#define RANDOM_BENCHMARK(func_name, lowerBound, upperBound) namespace {  \
+#define RANDOM_BENCHMARK(func_name, lowerBound, upperBound) namespace {                         \
     NONIUS_BENCHMARK((std::string(#func_name) + "(Random)"), [](nonius::chronometer meter) {    \
-        std::uniform_int_distribution<InputType> distribution(lowerBound, upperBound);    \
-        auto input = distribution(generator);   \
-        meter.measure([&input]() { return func_name(input); }); \
-    })  \
+        std::uniform_int_distribution<InputType> distribution(lowerBound, upperBound);          \
+        auto input = distribution(generator);                                                   \
+        meter.measure([&input]() { return func_name(input); });                                 \
+    })                                                                                          \
 }
 
 constexpr unsigned LONG_BITS_NUM = (sizeof(unsigned long) * CHAR_BIT);
 const unsigned HYPOTHETIC_MAX_STACK_DEPTH = 4096;
 
 #ifdef WANT_TESTS
-#define SIMPLE_TEST(func_name, input, expectedValue) namespace {            \
-    TEST(func_name##Test, Test##input) {                                    \
-        EXPECT_EQ(expectedValue, func_name(input)) << "Input: " << input;   \
-    }                                                                       \
+#define SIMPLE_TEST(func_name, testName, expectedValue, inputs...) namespace {  \
+    TEST(func_name##Test, testName) {                                           \
+        EXPECT_EQ(expectedValue, func_name(inputs)) << "Input: " << inputs;     \
+    }                                                                           \
 }
 #else
-#define SIMPLE_TEST(func_name, input, expectedValue) namespace {}
+#define SIMPLE_TEST(func_name, testName, expectedValue, inputs...) namespace {}
 #endif
 
 #ifdef WANT_TESTS
-#define MUTUAL_TEST(func1, func2, lowerBound, upperBound) namespace {                   \
-    TEST(MUTUAL_TEST, func1##vs##func2) {                                               \
+#define MUTUAL_RANDOM_TEST(func1, func2, lowerBound, upperBound) namespace {            \
+    TEST(MutualRandomTest, func1##vs##func2) {                                          \
         std::uniform_int_distribution<InputType> distribution(lowerBound, upperBound);  \
         auto input = distribution(generator);                                           \
         EXPECT_EQ(func1(input), func2(input)) << "Input: " << input;                    \
     }                                                                                   \
 }
 #else
-#define MUTUAL_TEST(func1, func2, lowerBound, upperBound) namespace {}
+#define MUTUAL_RANDOM_TEST(func1, func2, lowerBound, upperBound) namespace {}
 #endif
--- recurrent/josephus_problem.cpp
+++ recurrent/josephus_problem.cpp
@@ -63,6 +63,64 @@ InputType PenultimateSurvivorClosedform(const InputType n) {
     return ((n - m) << 1) + 1;
 }
 
+/** Authentic Josephus Problem
+ * @reference   Ronald Graham, Oren Patashnik, Donald Knuth.
+ *              Concrete Mathematics: A Foundation for Computer Science (2nd Edition). Chapter 3.3.
+ *
+ *  Authentic Josephus Problem in which every third person is eliminated, instead of every second.
+ */
+InputType JosephusProblem3(const InputType n) {
+    double D = 1.0;
+    const double BOUND = n * 2.0;
+    while (D <= BOUND) {
+        D = std::ceil(1.5 * D);
+    }
+    return 3 * n + 1 - static_cast<InputType>(D);
+}
+
+/** Kth Josephus problem
+ * @reference http://www.geeksforgeeks.org/josephus-problem-set-1-a-on-solution/
+ *
+ * There are n people standing in a circle waiting to be executed. The counting out begins at some
+ * point in the circle and proceeds around the circle in a fixed direction. In each step,
+ * a certain number of people are skipped and the next person is executed. The elimination proceeds
+ * around the circle (which is becoming smaller and smaller as the executed people are removed),
+ * until only the last person remains, who is given freedom. Given the total number of persons n
+ * and a number k which indicates that k-1 persons are skipped and kth person is killed in circle.
+ * The task is to choose the place in the initial circle so that you are the last one remaining and so survive.
+ */
+
+/**
+ * josephus(n, k) = (josephus(n - 1, k) + k-1) % n + 1;
+ * josephus(1, k) = 1.
+ *
+ * After the first person (kth from begining) is killed, n-1 persons are left. So we call josephus(n - 1, k)
+ * to get the position with n-1 persons. But the position returned by josephus(n - 1, k) will consider
+ * the position starting from k%n + 1. So, we must make adjustments to the position returned by josephus(n - 1, k).
+ */
+InputType JosephusKRecursive(const InputType n, const InputType k) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return (JosephusKRecursive(n - 1, k) + k - 1) % n + 1;
+    }
+}
+
+/**
+ * @reference   Ronald Graham, Oren Patashnik, Donald Knuth.
+ *              Concrete Mathematics: A Foundation for Computer Science (2nd Edition). Chapter 3.3.
+ */
+InputType JosephusKClosedform(const InputType n, const InputType k) {
+    double D = 1.0;
+    const double K_MINUS_ONE = static_cast<double>(k - 1);
+    const double BOUND = n * K_MINUS_ONE;
+    while (D <= BOUND) {
+        D = std::ceil(k / K_MINUS_ONE * D);
+    }
+    return k * n + 1 - static_cast<InputType>(D);
+}
+
+
 const InputType LOWER = 1;
 constexpr InputType UPPER = ~InputType(0);
 const InputType SAMPLE = 15;
@@ -72,20 +130,20 @@ SIMPLE_BENCHMARK(JosephusProblem2Recursive, UPPER);
 SIMPLE_BENCHMARK(JosephusProblem2Recursive, SAMPLE);
 RANDOM_BENCHMARK(JosephusProblem2Recursive, LOWER, UPPER);
 
-SIMPLE_TEST(JosephusProblem2Recursive, LOWER, 1);
-SIMPLE_TEST(JosephusProblem2Recursive, UPPER, UPPER);
-SIMPLE_TEST(JosephusProblem2Recursive, SAMPLE, 15);
+SIMPLE_TEST(JosephusProblem2Recursive, TestLOWER, 1, LOWER);
+SIMPLE_TEST(JosephusProblem2Recursive, TestUPPER, UPPER, UPPER);
+SIMPLE_TEST(JosephusProblem2Recursive, TestSAMPLE, 15, SAMPLE);
 
 SIMPLE_BENCHMARK(JosephusProblem2Closedform, LOWER);
 SIMPLE_BENCHMARK(JosephusProblem2Closedform, UPPER);
 SIMPLE_BENCHMARK(JosephusProblem2Closedform, SAMPLE);
 RANDOM_BENCHMARK(JosephusProblem2Closedform, LOWER, UPPER);
 
-SIMPLE_TEST(JosephusProblem2Closedform, LOWER, 1);
-SIMPLE_TEST(JosephusProblem2Closedform, UPPER, UPPER);
-SIMPLE_TEST(JosephusProblem2Closedform, SAMPLE, 15);
+SIMPLE_TEST(JosephusProblem2Closedform, TestLOWER, 1, LOWER);
+SIMPLE_TEST(JosephusProblem2Closedform, TestUPPER, UPPER, UPPER);
+SIMPLE_TEST(JosephusProblem2Closedform, TestSAMPLE, 15, SAMPLE);
 
-MUTUAL_TEST(JosephusProblem2Recursive, JosephusProblem2Closedform, LOWER, UPPER);
+MUTUAL_RANDOM_TEST(JosephusProblem2Recursive, JosephusProblem2Closedform, LOWER, UPPER);
 
 const InputType LOWER2 = 2;
 
@@ -94,6 +152,43 @@ SIMPLE_BENCHMARK(PenultimateSurvivorClosedform, UPPER);
 SIMPLE_BENCHMARK(PenultimateSurvivorClosedform, SAMPLE);
 RANDOM_BENCHMARK(PenultimateSurvivorClosedform, LOWER2, UPPER);
 
-SIMPLE_TEST(PenultimateSurvivorClosedform, LOWER2, 2);
-SIMPLE_TEST(PenultimateSurvivorClosedform, UPPER, UPPER / 2);
-SIMPLE_TEST(PenultimateSurvivorClosedform, SAMPLE, 7);
+SIMPLE_TEST(PenultimateSurvivorClosedform, TestLOWER2, 2, LOWER2);
+SIMPLE_TEST(PenultimateSurvivorClosedform, TestUPPER, UPPER / 2, UPPER);
+SIMPLE_TEST(PenultimateSurvivorClosedform, TestSAMPLE, 7, SAMPLE);
+
+constexpr InputType UPPER3 = ~InputType(0) / 4;
+
+SIMPLE_BENCHMARK(JosephusProblem3, LOWER);
+SIMPLE_BENCHMARK(JosephusProblem3, UPPER3);
+SIMPLE_BENCHMARK(JosephusProblem3, SAMPLE);
+RANDOM_BENCHMARK(JosephusProblem3, LOWER, UPPER3);
+
+SIMPLE_TEST(JosephusProblem3, TestLOWER, 1, LOWER);
+SIMPLE_TEST(JosephusProblem3, TestUPPER3, 2565990286715650046, UPPER3);
+SIMPLE_TEST(JosephusProblem3, TestSAMPLE, 5, SAMPLE);
+
+const InputType UPPER_K = HYPOTHETIC_MAX_STACK_DEPTH;
+
+SIMPLE_BENCHMARK(JosephusKRecursive, LOWER, UPPER_K);
+SIMPLE_BENCHMARK(JosephusKRecursive, LOWER, 2);
+SIMPLE_BENCHMARK(JosephusKRecursive, UPPER_K, UPPER_K);
+SIMPLE_BENCHMARK(JosephusKRecursive, UPPER_K, 2);
+SIMPLE_BENCHMARK(JosephusKRecursive, SAMPLE, UPPER_K);
+SIMPLE_BENCHMARK(JosephusKRecursive, SAMPLE, 2);
+
+SIMPLE_TEST(JosephusKRecursive, TestLOWER2, 1, LOWER, 2);
+SIMPLE_TEST(JosephusKRecursive, TestSAMPLE2, 15, SAMPLE, 2);
+SIMPLE_TEST(JosephusKRecursive, TestLOWER3, 1, LOWER, 3);
+SIMPLE_TEST(JosephusKRecursive, TestSAMPLE3, 5, SAMPLE, 3);
+
+SIMPLE_BENCHMARK(JosephusKClosedform, LOWER, UPPER_K);
+SIMPLE_BENCHMARK(JosephusKClosedform, LOWER, 2);
+SIMPLE_BENCHMARK(JosephusKClosedform, UPPER_K, UPPER_K);
+SIMPLE_BENCHMARK(JosephusKClosedform, UPPER_K, 2);
+SIMPLE_BENCHMARK(JosephusKClosedform, SAMPLE, UPPER_K);
+SIMPLE_BENCHMARK(JosephusKClosedform, SAMPLE, 2);
+
+SIMPLE_TEST(JosephusKClosedform, TestLOWER2, 1, LOWER, 2);
+SIMPLE_TEST(JosephusKClosedform, TestSAMPLE2, 15, SAMPLE, 2);
+SIMPLE_TEST(JosephusKClosedform, TestLOWER3, 1, LOWER, 3);
+SIMPLE_TEST(JosephusKClosedform, TestSAMPLE3, 5, SAMPLE, 3);
