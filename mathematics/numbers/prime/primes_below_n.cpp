b2ef323995005b05edc9287ecc443c69501e9d80
Yang Yang <yang.yang@netscout.com>
2016-10-07 14:40:47 -0700

    Mathematics: Add sum_of_primes_below_n
    Change-Id: I610baa5d09d75e09d146fd69c74791f9634c2675

--- mathematics/numbers/prime/primes_below_n.cpp
+++ mathematics/numbers/prime/primes_below_n.cpp
@@ -32,16 +32,30 @@ std::vector<InputType> SieveOfEratosthenes(const InputType N) {
     return output;
 }
 
+/** Sum of all prime numbers below N
+ */
+unsigned long long SumOfPrimesBelowN(const InputType N) {
+    auto primes = SieveOfEratosthenes(N);
+    return std::accumulate(primes.cbegin(), primes.cend(), 0ULL);
+}
+
 const InputType LOWER = 2;
 const InputType SAMPLE1 = 12;
 const InputType SAMPLE2 = 42;
 
 SIMPLE_BENCHMARK(SieveOfEratosthenes, LOWER);
 SIMPLE_BENCHMARK(SieveOfEratosthenes, SAMPLE1);
-RANDOM_BENCHMARK(SieveOfEratosthenes, LOWER, UINT_MAX);
 
 SIMPLE_TEST(SieveOfEratosthenes, TestLOWER, std::vector<InputType> {}, LOWER);
 const std::vector<InputType> RESULT1 = {2, 3, 5, 7, 11};
 SIMPLE_TEST(SieveOfEratosthenes, TestSAMPLE1, RESULT1, SAMPLE1);
 const std::vector<InputType> RESULT2 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41};
 SIMPLE_TEST(SieveOfEratosthenes, TestSAMPLE2, RESULT2, SAMPLE2);
+
+
+SIMPLE_BENCHMARK(SumOfPrimesBelowN, LOWER);
+SIMPLE_BENCHMARK(SumOfPrimesBelowN, SAMPLE1);
+
+SIMPLE_TEST(SumOfPrimesBelowN, TestLOWER, 0, LOWER);
+SIMPLE_TEST(SumOfPrimesBelowN, TestSAMPLE1, 28, SAMPLE1);
+SIMPLE_TEST(SumOfPrimesBelowN, TestSAMPLE2, 37550402023, 1000000);
