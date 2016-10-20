2f0c34759c46edeaa01a4c5ba8c3e549af38fb5a
Yang Yang <yang.yang@netscout.com>
2016-10-20 14:09:26 -0700

    Mathematics:Fix a bug for LargestPowerOfPDividesFactorialN()
    For large n, it will cause LargestPowerOfPDividesFactorialN() to fall
    into infinite loop, as i*=p may overflow. Use ULL instead of unsigned to
    prevent overflow.
    
    Also add more test cases.
    
    Change-Id: I3fff85faf132763d2bf317d1080a53764a3d2836

--- mathematics/numbers/prime/largest_power_of_p_divides_factorial_n.cpp
+++ mathematics/numbers/prime/largest_power_of_p_divides_factorial_n.cpp
@@ -14,7 +14,8 @@ typedef unsigned InputType;
  */
 InputType LargestPowerOfPDividesFactorialN(const InputType p, const InputType n) {
     InputType power = 0;
-    for (InputType i = p; i <= n; i *= p) {
+    //Use ULL to prevent overflow
+    for (unsigned long long i = p; i <= n; i *= p) {
         power += (n / i);
     }
     return power;
@@ -34,25 +35,39 @@ InputType LargestPowerOf2DividesFactorialNBuiltIn(const InputType n) {
 #endif
 
 
+const InputType LOWER = 0;
+const InputType UPPER = UINT_MAX;
+
+SIMPLE_BENCHMARK(LargestPowerOfPDividesFactorialN, 2, LOWER);
+SIMPLE_BENCHMARK(LargestPowerOfPDividesFactorialN, 2, UPPER);
 SIMPLE_BENCHMARK(LargestPowerOfPDividesFactorialN, 2, 10);
 
 SIMPLE_TEST(LargestPowerOfPDividesFactorialN, TestSAMPLE1, 0, 3, 2);
 SIMPLE_TEST(LargestPowerOfPDividesFactorialN, TestSAMPLE2, 8, 2, 10);
 SIMPLE_TEST(LargestPowerOfPDividesFactorialN, TestSAMPLE3, 4, 3, 9);
 SIMPLE_TEST(LargestPowerOfPDividesFactorialN, TestSAMPLE4, 97, 2, 100);
+SIMPLE_TEST(LargestPowerOfPDividesFactorialN, TestUPPER, LargestPowerOf2DividesFactorialN(UPPER),
+            2, UPPER);
 
+SIMPLE_BENCHMARK(LargestPowerOf2DividesFactorialN, LOWER);
+SIMPLE_BENCHMARK(LargestPowerOf2DividesFactorialN, UPPER);
 SIMPLE_BENCHMARK(LargestPowerOf2DividesFactorialN, 10);
 
 SIMPLE_TEST(LargestPowerOf2DividesFactorialN, TestSAMPLE1, 0, 0);
 SIMPLE_TEST(LargestPowerOf2DividesFactorialN, TestSAMPLE2, 8, 10);
 SIMPLE_TEST(LargestPowerOf2DividesFactorialN, TestSAMPLE3, 97, 100);
+SIMPLE_TEST(LargestPowerOf2DividesFactorialN, TestUPPER, UPPER - sizeof(UPPER) * CHAR_BIT, UPPER);
 
 #ifdef __GNUC__
+SIMPLE_BENCHMARK(LargestPowerOf2DividesFactorialNBuiltIn, LOWER);
+SIMPLE_BENCHMARK(LargestPowerOf2DividesFactorialNBuiltIn, UPPER);
 SIMPLE_BENCHMARK(LargestPowerOf2DividesFactorialNBuiltIn, 10);
 
 SIMPLE_TEST(LargestPowerOf2DividesFactorialNBuiltIn, TestSAMPLE1, 0, 0);
 SIMPLE_TEST(LargestPowerOf2DividesFactorialNBuiltIn, TestSAMPLE2, 8, 10);
 SIMPLE_TEST(LargestPowerOf2DividesFactorialNBuiltIn, TestSAMPLE3, 97, 100);
+SIMPLE_TEST(LargestPowerOf2DividesFactorialNBuiltIn, TestUPPER, UPPER - sizeof(UPPER) * CHAR_BIT,
+            UPPER);
 
 MUTUAL_RANDOM_TEST(LargestPowerOf2DividesFactorialN, LargestPowerOf2DividesFactorialNBuiltIn, 0,
                    96583);
