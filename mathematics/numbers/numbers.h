1fb803cd67b05f3586109df2606df73326a0f025
Yang Yang <yang.yang@netscout.com>
2016-10-27 14:22:56 -0700

    Mathematics:Numbers:Optimize LargeInteger Multiply
    Change-Id: Idbfe142a6e171dab07f8d1b458b30be5d5e99bbb

--- mathematics/numbers/numbers.h
+++ mathematics/numbers/numbers.h
@@ -3,16 +3,33 @@
 unsigned gcd(unsigned m, unsigned n);
 
 class LargeInteger {
+    typedef std::vector<unsigned char> NumData;
     //Number are stored in reverse digit order
-    std::vector<unsigned char> m_nums;
+    NumData m_nums;
     friend std::ostream &operator<<(std::ostream &os, const LargeInteger &num);
-
+    static const NumData::size_type BLOCK_SIZE = 8;
 public:
     explicit LargeInteger(const std::string &s_num);
+    LargeInteger(const LargeInteger &copy, const unsigned block_index);
     LargeInteger() = default;
     LargeInteger(const LargeInteger &copy) = default;
     LargeInteger(LargeInteger &&copy) = default;
     LargeInteger &operator=(const LargeInteger &) = default;
+
+    std::string ToString() const {
+        std::string str_num;
+        str_num.reserve(m_nums.size());
+        for (auto i_digit = m_nums.rbegin(); i_digit != m_nums.rend(); ++i_digit) {
+            str_num.push_back(*i_digit + '0');
+        }
+        return str_num;
+    }
+    unsigned ToInt() const {
+        if (isZero()) {
+            return 0;
+        }
+        return std::stoi(ToString());
+    }
 
     bool operator==(const LargeInteger &compare_to) const {
         if (isZero() and compare_to.isZero()) {
@@ -25,7 +42,7 @@ public:
     }
     LargeInteger operator+(const LargeInteger &addend) const;
     LargeInteger operator*(const LargeInteger &multiplicand) const;
-    LargeInteger operator*(const unsigned char multiplicand) const;
+    LargeInteger operator*(const unsigned multiplicand) const;
 private:
     static void addDigits(unsigned char &add_to, const unsigned char addend, unsigned char &carry);
     bool isZero() const {
