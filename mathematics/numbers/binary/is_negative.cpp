f1c5a559c46d3e6750b99e71850314b993d88703
Yang Yang <yang.yang@netscout.com>
2016-12-15 11:51:50 -0800

    Mathematics:Binary:Add is_negative
    Change-Id: Idfe61245756696a4d80e3077a955e816985d8a28

---
+++ mathematics/numbers/binary/is_negative.cpp
@@ -0,0 +1,27 @@
+#include "common_header.h"
+
+typedef int InputType;
+
+/** Compute the sign of an integer
+ *
+ * @reference   Compute the sign of an integer
+ *              https://graphics.stanford.edu/~seander/bithacks.html
+ */
+INT_BOOL IsNegativePortable(const InputType num) {
+    return 1 & ((unsigned int)num >> (sizeof(int) * CHAR_BIT - 1));
+}
+
+INT_BOOL IsNegative(const InputType num) {
+    return num < 0 ? TRUE : FALSE;
+}
+
+SIMPLE_BENCHMARK(IsNegativePortable, -1);
+SIMPLE_BENCHMARK(IsNegativePortable, 0);
+SIMPLE_BENCHMARK(IsNegativePortable, INT_MIN);
+SIMPLE_BENCHMARK(IsNegativePortable, INT_MAX);
+
+SIMPLE_TEST(IsNegativePortable, TestSample1, TRUE, -1);
+SIMPLE_TEST(IsNegativePortable, TestSample2, FALSE, 0);
+SIMPLE_TEST(IsNegativePortable, TestSample3, TRUE, INT_MIN);
+SIMPLE_TEST(IsNegativePortable, TestSample4, FALSE, INT_MAX);
+MUTUAL_RANDOM_TEST(IsNegativePortable, IsNegative, INT_MIN, INT_MAX);
