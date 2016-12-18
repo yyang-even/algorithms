fdd75859987deec9c991ca2cbf724f4a28143924
Yang Yang <yyang.even@gmail.com>
2016-12-17 22:10:38 -0800

    Math:Binary:Add sign_extending
    Change-Id: I25c75358123f1333021daf00bd68a2ab96f51225

---
+++ mathematics/numbers/binary/sign_extending.cpp
@@ -0,0 +1,103 @@
+#include "common_header.h"
+
+typedef int InputType;
+
+/** Sign extending
+ *
+ * @reference   Sean Eron Anderson. Bit Twiddling Hacks.
+ *              Sign extending from a constant bit-width
+ *              Sign extending from a variable bit-width
+ *              Sign extending from a variable bit-width in 3 operations
+ *              https://graphics.stanford.edu/~seander/bithacks.html
+ *
+ * Sign extension is automatic for built-in types, such as chars and ints.
+ * But suppose you have a signed two's complement number, x, that is stored
+ * using only b bits. Moreover, suppose you want to convert x to an int,
+ * which has more than b bits. A simple copy will work if x is positive,
+ * but if negative, the sign must be extended. For example, if we have only
+ * 4 bits to store a number, then -3 is represented as 1101 in binary.
+ * If we have 8 bits, then -3 is 11111101. The most-significant bit of
+ * the 4-bit representation is replicated sinistrally to fill in the
+ * destination when we convert to a representation with more bits;
+ * this is sign extending.
+ */
+template <unsigned B>
+inline InputType SignExtendConst(const InputType x) {
+    struct {
+    InputType x:
+        B;
+    } bits;
+    return bits.x = x;
+}
+InputType SignExtendConst4Bits(const InputType x) {
+    return SignExtendConst<4>(x);
+}
+
+template <unsigned B>
+inline InputType SignExtendVariable(InputType x) {
+    static const InputType mask = 1U << (B - 1);
+    x = x & ((1U << B) - 1);
+    return (x ^ mask) - mask;
+}
+InputType SignExtendVariable4Bits(InputType x) {
+    return SignExtendVariable<4>(x);
+}
+
+template <unsigned B>
+inline InputType SignExtendVariableDirty(const InputType x) {
+    static const InputType mask = CHAR_BIT * sizeof(x) - B;
+    return (x << mask) >> mask;
+}
+InputType SignExtendVariableDirty4Bits(InputType x) {
+    return SignExtendVariableDirty<4>(x);
+}
+
+
+const InputType SAMPLE1 = 0b0000; //0
+const InputType SAMPLE2 = 0b0001; //1
+const InputType SAMPLE3 = 0b1111; //-1
+const InputType SAMPLE4 = 0b1000; //-8
+const InputType SAMPLE5 = 0b0111; //7
+
+SIMPLE_BENCHMARK(SignExtendConst4Bits, 1);
+SIMPLE_BENCHMARK(SignExtendConst4Bits, 0);
+SIMPLE_BENCHMARK(SignExtendConst4Bits, INT_MIN);
+SIMPLE_BENCHMARK(SignExtendConst4Bits, UINT_MAX);
+
+SIMPLE_TEST(SignExtendConst4Bits, TestSample1, 0, SAMPLE1);
+SIMPLE_TEST(SignExtendConst4Bits, TestSample2, 1, SAMPLE2);
+SIMPLE_TEST(SignExtendConst4Bits, TestSample3, -1, SAMPLE3);
+SIMPLE_TEST(SignExtendConst4Bits, TestSample4, -8, SAMPLE4);
+SIMPLE_TEST(SignExtendConst4Bits, TestSample5, 7, SAMPLE5);
+SIMPLE_TEST(SignExtendConst4Bits, TestSample6, 0, INT_MIN);
+SIMPLE_TEST(SignExtendConst4Bits, TestSample7, -1, INT_MAX);
+
+SIMPLE_BENCHMARK(SignExtendVariable4Bits, 1);
+SIMPLE_BENCHMARK(SignExtendVariable4Bits, 0);
+SIMPLE_BENCHMARK(SignExtendVariable4Bits, INT_MIN);
+SIMPLE_BENCHMARK(SignExtendVariable4Bits, UINT_MAX);
+
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample1, 0, SAMPLE1);
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample2, 1, SAMPLE2);
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample3, -1, SAMPLE3);
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample4, -8, SAMPLE4);
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample5, 7, SAMPLE5);
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample6, 0, INT_MIN);
+SIMPLE_TEST(SignExtendVariable4Bits, TestSample7, -1, INT_MAX);
+
+SIMPLE_BENCHMARK(SignExtendVariableDirty4Bits, 1);
+SIMPLE_BENCHMARK(SignExtendVariableDirty4Bits, 0);
+SIMPLE_BENCHMARK(SignExtendVariableDirty4Bits, INT_MIN);
+SIMPLE_BENCHMARK(SignExtendVariableDirty4Bits, UINT_MAX);
+
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample1, 0, SAMPLE1);
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample2, 1, SAMPLE2);
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample3, -1, SAMPLE3);
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample4, -8, SAMPLE4);
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample5, 7, SAMPLE5);
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample6, 0, INT_MIN);
+SIMPLE_TEST(SignExtendVariableDirty4Bits, TestSample7, -1, INT_MAX);
+
+MUTUAL_RANDOM_TEST(SignExtendConst4Bits, SignExtendVariable4Bits, INT_MIN, INT_MAX);
+MUTUAL_RANDOM_TEST(SignExtendConst4Bits, SignExtendVariableDirty4Bits, INT_MIN, INT_MAX);
+MUTUAL_RANDOM_TEST(SignExtendVariableDirty4Bits, SignExtendVariable4Bits, INT_MIN, INT_MAX);
