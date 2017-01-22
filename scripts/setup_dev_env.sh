8f10d9956b7e544f678522bb0c0cb52ba1654ff1
Yang Yang <yyang.even@gmail.com>
2017-01-22 13:03:24 -0800

    Scripts: Add setup_dev_env.sh
    Change-Id: Ic225359bb1e9585a542799348d196650259cd097

---
+++ scripts/setup_dev_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# This script setup develop environment by installing required libraries
+#
+
+Echo_Error() {
+    cat <<< "$@" 1>&2;
+}
+
+Install_or_Update_Packages() {
+    operating_system=$(uname)
+    if [ $operating_system == "Linux" ]; then
+        if [ -f /etc/redhat-release ]; then
+            sudo yum --assumeyes install "$@"
+        elif [ -f /etc/debian_version ]; then
+            sudo apt-get --yes install "$@"
+        else
+            Echo_Error "Error: Unsupported distribution"
+            cat /etc/*-release
+            return 1
+        fi
+    else
+        Echo_Error "Error: Unsupported OS: " $operating_system
+        return 1
+    fi
+    return $?
+}
+
+
+Install_or_Update_Packages libboost-all-dev
