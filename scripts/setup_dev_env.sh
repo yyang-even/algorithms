55a6643e00bbbf188f38518f5d34d37c51949493
Yang Yang <yang.yang@netscout.com>
2017-02-02 14:11:28 -0800

    Script: Add pre-commit git hook
    Change-Id: I9a02b6eab70f550cc49689570e2ecca45bedf989

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -27,5 +27,8 @@ Install_or_Update_Packages() {
     return $?
 }
 
-
+# Install third-party libraries
 Install_or_Update_Packages libboost-all-dev
+# Copy git hooks
+SCRIPT_DIR=$(dirname "$0")
+cp --interactive $SCRIPT_DIR/git_hooks/* $SCRIPT_DIR/../.git/hooks/
