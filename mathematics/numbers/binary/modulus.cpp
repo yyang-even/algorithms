fb1de2cb37b1cb70a766fe3718736ccdad2745ac
Yang Yang <yang.yang@netscout.com>
2017-03-21 12:06:34 -0700

    Binary: Add more motheds to modulus.cpp
    Change-Id: I6c65037de33e074fa5bb4b79ae76f338160c34c9

--- mathematics/numbers/binary/modulus.cpp
+++ mathematics/numbers/binary/modulus.cpp
@@ -7,11 +7,113 @@ typedef unsigned InputType;
  * @reference   Sean Eron Anderson. Bit Twiddling Hacks.
  *              Compute modulus division by 1 << s without a division operator
  *              https://graphics.stanford.edu/~seander/bithacks.html
- *
- * Swap two values without using a temporary variable.
  */
-InputType Modulus2Power(const unsigned num, const unsigned s) {
+InputType Modulus2Power(const InputType num, const InputType s) {
     return num & ((1U << s) - 1);
+}
+
+
+/** Compute modulus division by (1 << s) - 1 without a division operator
+ *
+ * @reference   Sean Eron Anderson. Bit Twiddling Hacks.
+ *              Compute modulus division by (1 << s) - 1 without a division operator
+ *              https://graphics.stanford.edu/~seander/bithacks.html
+ */
+InputType Modulus2PowerMinus1(InputType num, const InputType s) {
+    InputType result = num;
+    InputType divisor = (1U << s) - 1;
+
+    for (; num > divisor; num = result) {
+        for (result = 0; num; num >>= s) {
+            result += num & divisor;
+        }
+    }
+    // Now result is a value from 0 to divisor, but since with modulus division
+    // we want result to be 0 when it is divisor.
+    return result == divisor ? 0 : result;
+}
+
+
+/** Compute modulus division by (1 << s) - 1 in parallel without a division operator
+ *
+ * @reference   Sean Eron Anderson. Bit Twiddling Hacks.
+ *              Compute modulus division by (1 << s) - 1 in parallel without a division operator
+ *              https://graphics.stanford.edu/~seander/bithacks.html
+ */
+InputType Modulus2PowerMinus1Parallel(InputType num, const InputType s) {
+    // The following is for a word size of 32 bits!
+    static_assert(sizeof(InputType) * CHAR_BIT == 32, "InputType is not 32 bits.");
+
+    static const unsigned int M[] = {
+        0x00000000, 0x55555555, 0x33333333, 0xc71c71c7,
+        0x0f0f0f0f, 0xc1f07c1f, 0x3f03f03f, 0xf01fc07f,
+        0x00ff00ff, 0x07fc01ff, 0x3ff003ff, 0xffc007ff,
+        0xff000fff, 0xfc001fff, 0xf0003fff, 0xc0007fff,
+        0x0000ffff, 0x0001ffff, 0x0003ffff, 0x0007ffff,
+        0x000fffff, 0x001fffff, 0x003fffff, 0x007fffff,
+        0x00ffffff, 0x01ffffff, 0x03ffffff, 0x07ffffff,
+        0x0fffffff, 0x1fffffff, 0x3fffffff, 0x7fffffff
+    };
+
+    static const unsigned int Q[][6] = {
+        { 0,  0,  0,  0,  0,  0}, {16,  8,  4,  2,  1,  1}, {16,  8,  4,  2,  2,  2},
+        {15,  6,  3,  3,  3,  3}, {16,  8,  4,  4,  4,  4}, {15,  5,  5,  5,  5,  5},
+        {12,  6,  6,  6 , 6,  6}, {14,  7,  7,  7,  7,  7}, {16,  8,  8,  8,  8,  8},
+        { 9,  9,  9,  9,  9,  9}, {10, 10, 10, 10, 10, 10}, {11, 11, 11, 11, 11, 11},
+        {12, 12, 12, 12, 12, 12}, {13, 13, 13, 13, 13, 13}, {14, 14, 14, 14, 14, 14},
+        {15, 15, 15, 15, 15, 15}, {16, 16, 16, 16, 16, 16}, {17, 17, 17, 17, 17, 17},
+        {18, 18, 18, 18, 18, 18}, {19, 19, 19, 19, 19, 19}, {20, 20, 20, 20, 20, 20},
+        {21, 21, 21, 21, 21, 21}, {22, 22, 22, 22, 22, 22}, {23, 23, 23, 23, 23, 23},
+        {24, 24, 24, 24, 24, 24}, {25, 25, 25, 25, 25, 25}, {26, 26, 26, 26, 26, 26},
+        {27, 27, 27, 27, 27, 27}, {28, 28, 28, 28, 28, 28}, {29, 29, 29, 29, 29, 29},
+        {30, 30, 30, 30, 30, 30}, {31, 31, 31, 31, 31, 31}
+    };
+
+    static const unsigned int R[][6] = {
+        {0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000},
+        {0x0000ffff, 0x000000ff, 0x0000000f, 0x00000003, 0x00000001, 0x00000001},
+        {0x0000ffff, 0x000000ff, 0x0000000f, 0x00000003, 0x00000003, 0x00000003},
+        {0x00007fff, 0x0000003f, 0x00000007, 0x00000007, 0x00000007, 0x00000007},
+        {0x0000ffff, 0x000000ff, 0x0000000f, 0x0000000f, 0x0000000f, 0x0000000f},
+        {0x00007fff, 0x0000001f, 0x0000001f, 0x0000001f, 0x0000001f, 0x0000001f},
+        {0x00000fff, 0x0000003f, 0x0000003f, 0x0000003f, 0x0000003f, 0x0000003f},
+        {0x00003fff, 0x0000007f, 0x0000007f, 0x0000007f, 0x0000007f, 0x0000007f},
+        {0x0000ffff, 0x000000ff, 0x000000ff, 0x000000ff, 0x000000ff, 0x000000ff},
+        {0x000001ff, 0x000001ff, 0x000001ff, 0x000001ff, 0x000001ff, 0x000001ff},
+        {0x000003ff, 0x000003ff, 0x000003ff, 0x000003ff, 0x000003ff, 0x000003ff},
+        {0x000007ff, 0x000007ff, 0x000007ff, 0x000007ff, 0x000007ff, 0x000007ff},
+        {0x00000fff, 0x00000fff, 0x00000fff, 0x00000fff, 0x00000fff, 0x00000fff},
+        {0x00001fff, 0x00001fff, 0x00001fff, 0x00001fff, 0x00001fff, 0x00001fff},
+        {0x00003fff, 0x00003fff, 0x00003fff, 0x00003fff, 0x00003fff, 0x00003fff},
+        {0x00007fff, 0x00007fff, 0x00007fff, 0x00007fff, 0x00007fff, 0x00007fff},
+        {0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff, 0x0000ffff},
+        {0x0001ffff, 0x0001ffff, 0x0001ffff, 0x0001ffff, 0x0001ffff, 0x0001ffff},
+        {0x0003ffff, 0x0003ffff, 0x0003ffff, 0x0003ffff, 0x0003ffff, 0x0003ffff},
+        {0x0007ffff, 0x0007ffff, 0x0007ffff, 0x0007ffff, 0x0007ffff, 0x0007ffff},
+        {0x000fffff, 0x000fffff, 0x000fffff, 0x000fffff, 0x000fffff, 0x000fffff},
+        {0x001fffff, 0x001fffff, 0x001fffff, 0x001fffff, 0x001fffff, 0x001fffff},
+        {0x003fffff, 0x003fffff, 0x003fffff, 0x003fffff, 0x003fffff, 0x003fffff},
+        {0x007fffff, 0x007fffff, 0x007fffff, 0x007fffff, 0x007fffff, 0x007fffff},
+        {0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff},
+        {0x01ffffff, 0x01ffffff, 0x01ffffff, 0x01ffffff, 0x01ffffff, 0x01ffffff},
+        {0x03ffffff, 0x03ffffff, 0x03ffffff, 0x03ffffff, 0x03ffffff, 0x03ffffff},
+        {0x07ffffff, 0x07ffffff, 0x07ffffff, 0x07ffffff, 0x07ffffff, 0x07ffffff},
+        {0x0fffffff, 0x0fffffff, 0x0fffffff, 0x0fffffff, 0x0fffffff, 0x0fffffff},
+        {0x1fffffff, 0x1fffffff, 0x1fffffff, 0x1fffffff, 0x1fffffff, 0x1fffffff},
+        {0x3fffffff, 0x3fffffff, 0x3fffffff, 0x3fffffff, 0x3fffffff, 0x3fffffff},
+        {0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff}
+    };
+
+    const unsigned int divisor = (1 << s) - 1; // so divisor is either 1, 3, 7, 15, 31, ...).
+    unsigned int result;
+
+    result = (num & M[s]) + ((num >> s) & M[s]);
+
+    for (const unsigned int *q = &Q[s][0], * r = &R[s][0]; result > divisor; q++, r++) {
+        result = (result >> *q) + (result & *r);
+    }
+    // OR, less portably: result & -((signed)(result - divisor) >> s);
+    return result == divisor ? 0 : result;
 }
 
 
@@ -24,3 +126,17 @@ SIMPLE_TEST(Modulus2Power, TestLOWER, LOWER, LOWER, LOWER);
 SIMPLE_TEST(Modulus2Power, TestUPPER, UPPER, UPPER, UPPER);
 SIMPLE_TEST(Modulus2Power, TestSAMPLE1, 51, 179, 7);
 SIMPLE_TEST(Modulus2Power, TestSAMPLE2, 0, 708, 2);
+
+SIMPLE_BENCHMARK(Modulus2PowerMinus1, 1, UPPER);
+
+SIMPLE_TEST(Modulus2PowerMinus1, TestLOWER, LOWER, LOWER, LOWER);
+SIMPLE_TEST(Modulus2PowerMinus1, TestUPPER, UPPER, UPPER, UPPER);
+SIMPLE_TEST(Modulus2PowerMinus1, TestSAMPLE1, 52, 179, 7);
+SIMPLE_TEST(Modulus2PowerMinus1, TestSAMPLE2, 0, 708, 2);
+
+SIMPLE_BENCHMARK(Modulus2PowerMinus1Parallel, 1, UPPER);
+
+SIMPLE_TEST(Modulus2PowerMinus1Parallel, TestLOWER, LOWER, LOWER, LOWER);
+SIMPLE_TEST(Modulus2PowerMinus1Parallel, TestUPPER, UPPER, UPPER, UPPER);
+SIMPLE_TEST(Modulus2PowerMinus1Parallel, TestSAMPLE1, 52, 179, 7);
+SIMPLE_TEST(Modulus2PowerMinus1Parallel, TestSAMPLE2, 0, 708, 2);
