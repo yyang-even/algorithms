9598a8ead909859c144385431fc995285900c07b
Yang Yang <yyang.even@gmail.com>
2017-07-10 23:32:30 -0700

    Binary: Add round_up_to_power_of_2.cpp
    Change-Id: I4b264976b9ecd85bf1b1285a85538f81da17f111

---
+++ mathematics/numbers/binary/round_up_to_power_of_2.cpp
@@ -0,0 +1,66 @@
+#include "common_header.h"
+
+typedef unsigned InputType;
+
+/** Round up to the next highest power of 2 by float casting
+ *
+ * @reference   Sean Eron Anderson. Bit Twiddling Hacks.
+ *              Round up to the next highest power of 2 by float casting
+ *              https://graphics.stanford.edu/~seander/bithacks.html
+ *
+ * So num=3 -> r=4; num=8 -> r=8
+ */
+InputType RoundUpToPowerOf2Float(const InputType num) {
+    static_assert(sizeof(InputType) * CHAR_BIT == 32, "InputType is not 32 bits.");
+
+    if (num) {
+        const float f_num = (float)num;
+        const auto temp = 1U << ((*(unsigned int *)&f_num >> 23) - 0x7f);
+        return temp << (temp < num);
+    } else {
+        return 1;
+    }
+}
+
+
+/** Round up to the next highest power of 2
+ *
+ * @reference   Sean Eron Anderson. Bit Twiddling Hacks.
+ *              Round up to the next highest power of 2
+ *              https://graphics.stanford.edu/~seander/bithacks.html
+ */
+InputType RoundUpToPowerOf2(InputType num) {
+    if (num) {
+        --num;
+        num |= num >> 1;
+        num |= num >> 2;
+        num |= num >> 4;
+        num |= num >> 8;
+        num |= num >> 16;
+        return ++num;
+    } else {
+        return 1;
+    }
+}
+
+
+constexpr auto LOWER = std::numeric_limits<InputType>::min();
+constexpr auto UPPER = 1 << (CHAR_BIT *sizeof(InputType) - 1);
+
+SIMPLE_BENCHMARK(RoundUpToPowerOf2Float, UPPER);
+
+SIMPLE_TEST(RoundUpToPowerOf2Float, TestLOWER, 1, LOWER);
+SIMPLE_TEST(RoundUpToPowerOf2Float, TestUPPER, UPPER, UPPER);
+SIMPLE_TEST(RoundUpToPowerOf2Float, TestSAMPLE1, 4, 3);
+SIMPLE_TEST(RoundUpToPowerOf2Float, TestSAMPLE2, 8, 8);
+SIMPLE_TEST(RoundUpToPowerOf2Float, TestSAMPLE3, UPPER, UPPER - 1);
+
+SIMPLE_BENCHMARK(RoundUpToPowerOf2, UPPER);
+
+SIMPLE_TEST(RoundUpToPowerOf2, TestLOWER, 1, LOWER);
+SIMPLE_TEST(RoundUpToPowerOf2, TestUPPER, UPPER, UPPER);
+SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE1, 4, 3);
+SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE2, 8, 8);
+SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE3, UPPER, UPPER - 1);
+
+MUTUAL_RANDOM_TEST(RoundUpToPowerOf2Float, RoundUpToPowerOf2, LOWER, UPPER);
