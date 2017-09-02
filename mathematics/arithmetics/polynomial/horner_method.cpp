054976f206044d0be2693c0211ad1f4f3ed5a82d
Yang Yang <yyang.even@gmail.com>
2017-09-01 20:03:59 -0700

    Polynomial: Add horner_method
    Change-Id: I7b9bb83915c5adb254e977e43724a82719743b51

---
+++ mathematics/arithmetics/polynomial/horner_method.cpp
@@ -0,0 +1,44 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+template <std::size_t N>
+using ArrayType = std::array<int, N>;
+
+
+/** Horner’s Method for Polynomial Evaluation
+ *
+ * Given a polynomial of the form c(n)x^n + c(n-1)x^(n-1) + c(n-2)x^(n-2) + ... + c1x + c0
+ * and a value of x, find the value of polynomial for a given value of x.
+ * Here cn, cn-1, ... are integers (may be negative) and n is a positive integer.
+ * Input is in the form of an array say poly[] where poly[0] represents coefficient
+ * for x^n and poly[1] represents coefficient for x^(n-1) and so on.
+ *
+ *
+ * @reference   http://www.geeksforgeeks.org/horners-method-polynomial-evaluation/
+ *
+ * @complexity: O(n)
+ */
+template <std::size_t N>
+auto HornerMethod(ArrayType<N> coefficients, const int x) {
+    int result = 0;
+
+    for (const auto coefficient : coefficients) {
+        result = coefficient + result * x;
+    }
+
+    return result;
+}
+
+
+constexpr ArrayType<0> VALUES1 = {};
+constexpr ArrayType<1> VALUES2 = {1};
+constexpr ArrayType<2> VALUES3 = {1, 0};
+constexpr ArrayType<4> VALUES4 = {2, -6, 2, -1};
+constexpr ArrayType<4> VALUES5 = {2, 0, 3, 1};
+
+SIMPLE_TEST(HornerMethod, TestSAMPLE1, 0, VALUES1, 3);
+SIMPLE_TEST(HornerMethod, TestSAMPLE2, 1, VALUES2, 3);
+SIMPLE_TEST(HornerMethod, TestSAMPLE3, 3, VALUES3, 3);
+SIMPLE_TEST(HornerMethod, TestSAMPLE4, 5, VALUES4, 3);
+SIMPLE_TEST(HornerMethod, TestSAMPLE5, 23, VALUES5, 2);
