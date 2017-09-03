4fcc2d40a40bed5164a1901e598770a30ea80c52
Yang Yang <yyang.even@gmail.com>
2017-09-03 15:45:31 -0700

    Arithmetics: Add power.cpp
    Change-Id: I6a30b977bca8df7b70349b38d98f6086ea33d288

---
+++ mathematics/arithmetics/power/power.cpp
@@ -0,0 +1,60 @@
+#include "common_header.h"
+
+/** Write a program to calculate pow(x,n)
+ * @reference   Write a program to calculate pow(x,n)
+ *              http://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
+ *
+ * Given two integers x and n, write a function to compute x^n.
+ * We may assume that x and n are small and overflow doesn’t happen.
+ *
+ * @complexity: O(lgn)
+ */
+long Power(const long x, const unsigned int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    const auto x_power_half_n = Power(x, n / 2);
+    if (n % 2 == 0) {
+        return x_power_half_n * x_power_half_n;
+    } else {
+        return x * x_power_half_n * x_power_half_n;
+    }
+}
+
+double Power(const double x, const int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    const auto x_power_half_n = Power(x, n / 2);
+    if (n % 2 == 0) {
+        return x_power_half_n * x_power_half_n;
+    } else {
+        if (n > 0) {
+            return x * x_power_half_n * x_power_half_n;
+        } else {
+            return x_power_half_n * x_power_half_n / x;
+        }
+    }
+}
+
+
+const long LOWER = 0;
+
+SIMPLE_BENCHMARK(Power, LOWER, LOWER);
+SIMPLE_BENCHMARK(Power, 2, 7);
+
+SIMPLE_TEST(Power, TestLOWER, 1, LOWER, LOWER);
+SIMPLE_TEST(Power, TestSAMPLE1, 8, 2, 3);
+SIMPLE_TEST(Power, TestSAMPLE2, 49, 7, 2);
+
+
+const double DOUBLE_LOWER = 0.0;
+SIMPLE_BENCHMARK(Power, DOUBLE_LOWER, DOUBLE_LOWER);
+SIMPLE_BENCHMARK(Power, 2.0, 7);
+
+SIMPLE_TEST(Power, TestDoubleLOWER, 1.0, DOUBLE_LOWER, DOUBLE_LOWER);
+SIMPLE_TEST(Power, TestDoubleSAMPLE1, 8.0, 2.0, 3);
+SIMPLE_TEST(Power, TestDoubleSAMPLE2, 49.0, 7.0, 2);
+SIMPLE_TEST(Power, TestDoubleSAMPLE3, 0.125, 2.0, -3);
