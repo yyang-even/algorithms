d119316bee1da96d652dec6e831d1c7938ad836a
Yang Yang <yyang.even@gmail.com>
2018-07-15 16:40:02 -0700

    Probabilistic: Add fair_coin_from_biased_coin.cpp
    Change-Id: Iba3d1b45a83851bc3122344a9bf69ba2bbf34ac0

---
+++ mathematics/probabilistic/fair_coin_from_biased_coin.cpp
@@ -0,0 +1,54 @@
+#include "common_header.h"
+
+/** Make a fair coin from a biased coin
+ *
+ * @reference   Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+ *              Introduction to Algorithms, Third Edition. Chapter 5.1. Exercise 5.1-3.
+ *
+ *              https://www.geeksforgeeks.org/print-0-and-1-with-50-probability/
+ *
+ * You are given a function foo() that represents a biased coin.
+ * When foo() is called, it returns 0 with 60% probability, and
+ * 1 with 40% probability. Write a new function that returns 0
+ * and 1 with 50% probability each. Your function should use only
+ * foo(), no other library method.
+ */
+bool foo() {
+    static const std::vector<bool> SAMPLES = {false, false, false,
+                                              false, false, false,
+                                              true, true, true, true
+                                             };
+    static std::default_random_engine generator;
+    static std::uniform_int_distribution<int> distribution(0, SAMPLES.size() - 1);
+
+    return SAMPLES[distribution(generator)];
+}
+bool MakeFairCoinfromBiasedCoin() {
+    bool coin1 = foo();
+    bool coin2 = foo();
+    if (coin1 ^ coin2) {
+        return coin2;
+    } else {
+        return MakeFairCoinfromBiasedCoin();
+    }
+}
+
+
+bool TestMakeFairCoinfromBiasedCoin() {
+    static const int SAMPLE_SIZE = 100000;
+    static const int HALF_SAMPLE_SIZE = SAMPLE_SIZE / 2;
+    static const double ERROR_RATE = 0.001;
+    static const int TOLERATION = SAMPLE_SIZE * ERROR_RATE;
+
+    int counter = 0;
+    for (int i = 0; i < SAMPLE_SIZE; ++i) {
+        counter += MakeFairCoinfromBiasedCoin();
+    }
+
+    return (HALF_SAMPLE_SIZE - TOLERATION) <= counter and (HALF_SAMPLE_SIZE + TOLERATION) >= counter;
+}
+
+
+SIMPLE_BENCHMARK(MakeFairCoinfromBiasedCoin);
+
+SIMPLE_TEST0(TestMakeFairCoinfromBiasedCoin, TestSample, true);
