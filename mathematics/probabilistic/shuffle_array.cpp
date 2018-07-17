648e8b0f09f125b04c6dead356fb684f3f5b4530
Yang Yang <yyang.even@gmail.com>
2018-07-16 22:48:18 -0700

    Probabilistic: Add ShuffleArrayBySorting() to shuffle_array
    Change-Id: I70ae228624d93bca9a2958222f5eac8eaeb405b3

--- mathematics/probabilistic/shuffle_array.cpp
+++ mathematics/probabilistic/shuffle_array.cpp
@@ -26,15 +26,46 @@ void ShuffleArrayInPlace(ArrayType<N> &array) {
     }
 }
 
+/**
+ * @reference   Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+ *              Introduction to Algorithms, Third Edition. Chapter 5.3.
+ *
+ * @complexity  O(nlgn)
+ */
+template <std::size_t N>
+auto ShuffleArrayBySorting(const ArrayType<N> &array) {
+    const int PRIORITY_BOUND = pow(array.size(), 3.0);
+
+    std::vector<std::pair<int, typename ArrayType<N>::size_type> > priorities;
+    for (typename ArrayType<N>::size_type i = 0; i < array.size(); ++i) {
+        priorities.emplace_back(Random_Number(0, PRIORITY_BOUND), i);
+    }
+
+    std::sort(priorities.begin(), priorities.end(), [](const auto & a, const auto & b) {
+        return a.first < b.first;
+    });
+
+    ArrayType<N> output;
+    for (typename ArrayType<N>::size_type i = 0; i < array.size(); ++i) {
+        output[i] = array[priorities[i].second];
+    }
+
+    return output;
+}
+
 
 ArrayType<8> VALUES = {1, 2, 3, 4, 5, 6, 7, 8};
 SIMPLE_BENCHMARK(ShuffleArrayInPlace, VALUES);
+SIMPLE_BENCHMARK(ShuffleArrayBySorting, VALUES);
 
 
 #ifdef WANT_TERMINAL_APP
 int main(int argc, char **argv) {
     ShuffleArrayInPlace(VALUES);
-    std::cout << "Shuffled array: " << VALUES << std::endl;
+    std::cout << "Shuffled array in place: " << VALUES << std::endl;
+
+    const auto output = ShuffleArrayBySorting(VALUES);
+    std::cout << "Shuffled array by sorting: " << output << std::endl;
 
     return EXIT_SUCCESS;
 }
