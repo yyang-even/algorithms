052053814fa0866684e05c765d11fcf88395a240
Yang Yang <yang.yang@netscout.com>
2018-07-18 15:59:48 -0700

    Probabilistic: Add RandomNumberFromStream()
    Change-Id: Ib3cd0b92f8eaab524314cf5d4d5f31ecf65c22fd

--- mathematics/probabilistic/reservoir_sampling.cpp
+++ mathematics/probabilistic/reservoir_sampling.cpp
@@ -19,32 +19,49 @@ using ArrayType = std::array<int, N>;
  *
  * So we are given a big array (or stream) of numbers (to simplify), and we need to write
  * an efficient function to randomly select k numbers where 1 <= k <= n.
- * Let the input array be stream[]. 
+ * Let the input array be stream[].
  *
  * @complexity  O(n)
  */
 template <std::size_t N>
-auto ReservoirSampling(const ArrayType<N> &stream, const typename ArrayType<N>::size_type k){
-    assert (k < stream.size());
+auto ReservoirSampling(const ArrayType<N> &stream, const typename ArrayType<N>::size_type k) {
+    assert(k < stream.size());
 
     std::vector<typename ArrayType<N>::value_type> samples {stream.cbegin(), stream.cbegin() + k};
-    for(auto i = k; i < stream.size(); ++i) {
+    for (auto i = k; i < stream.size(); ++i) {
         const auto j = Random_Number<typename ArrayType<N>::size_type>(0, i);
-        if (j < k)
+        if (j < k) {
             samples[j] = stream[i];
+        }
     }
 
     return samples;
 }
 
 
+/** Select a random number from stream, with O(1) space
+ *
+ * @reference   https://www.geeksforgeeks.org/select-a-random-number-from-stream-with-o1-space/
+ *
+ * Given a stream of numbers, generate a random number from the stream.
+ * You are allowed to use only O(1) space and the input is in the form of stream,
+ * so can’t store the previously seen numbers.
+ */
+template <std::size_t N>
+auto RandomNumberFromStream(const ArrayType<N> &stream) {
+    return ReservoirSampling(stream, 1).back();
+}
+
+
 ArrayType<8> VALUES = {1, 2, 3, 4, 5, 6, 7, 8};
 SIMPLE_BENCHMARK(ReservoirSampling, VALUES, 3);
+SIMPLE_BENCHMARK(RandomNumberFromStream, VALUES);
 
 
 #ifdef WANT_TERMINAL_APP
 int main(int argc, char **argv) {
     std::cout << "Selected Samples: " << ReservoirSampling(VALUES, 3) << std::endl;
+    std::cout << "Selected Random Number from Stream: " << RandomNumberFromStream(VALUES) << std::endl;
 
     return EXIT_SUCCESS;
 }
