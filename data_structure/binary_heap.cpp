a67da2424e41ccd75453d98d76f4c7a0949b4126
Yang Yang <yang.yang@netscout.com>
2018-07-26 16:53:45 -0700

    DataStructure: Add binary_heap
    Change-Id: I2d987e1fa5aa16c63e61b3e388c61c4b4df48106

---
+++ data_structure/binary_heap.cpp
@@ -0,0 +1,25 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+#include "binary_heap.h"
+
+auto testBinaryHeapPushAndPop(const MinHeap::ArrayType &array) {
+    MinHeap heap;
+    for (const auto elem : array) {
+        heap.Push(elem);
+    }
+
+    MinHeap::ArrayType sorted_array;
+    while (not heap.Empty()) {
+        sorted_array.push_back(heap.Pop());
+    }
+
+    return sorted_array;
+}
+
+
+const MinHeap::ArrayType SAMPLE_ARRAY {1, 0, 8, 6, 2, 3, 7, 4, 5, 9};
+const MinHeap::ArrayType EXPECTED_ARRAY {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+SIMPLE_TEST(testBinaryHeapPushAndPop, TestSample, EXPECTED_ARRAY, SAMPLE_ARRAY);
