06e94898ac0812654b11d18ccd7ab19cf0b4fb34
Yang Yang <yang.yang@netscout.com>
2018-07-31 15:40:48 -0700

    DataStructure: Add priority_queue
    Change-Id: I5e82b73d2bafaad699a3f8aa27da6515c0859cec

---
+++ data_structure/priority_queue.h
@@ -0,0 +1,15 @@
+#include "common_header.h"
+
+#include "binary_heap.h"
+
+/** Priority Queue
+ *
+ * @reference   Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+ *              Introduction to Algorithms, Third Edition. Chapter 6.5. Exercises 6.5-3.
+ *
+ *              https://www.geeksforgeeks.org/priority-queue-set-1-introduction/
+ *              Priority Queue in C++ Standard Template Library (STL)
+ *              https://www.geeksforgeeks.org/priority-queue-in-cpp-stl/
+ */
+using MaxPriorityQueue = MaxHeap;
+using MinPriorityQueue = MinHeap;
