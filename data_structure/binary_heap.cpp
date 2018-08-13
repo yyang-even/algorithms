21c1592b548858641ea9d702ba94234ba458bc07
Yang Yang <yang.yang@netscout.com>
2018-08-13 12:47:02 -0700

    DataStructure: Implement Heap Delete
    Change-Id: I7b52337a35e974bc70a625de370da69d9a1792f4

--- data_structure/binary_heap.cpp
+++ data_structure/binary_heap.cpp
@@ -4,11 +4,13 @@
 
 #include "binary_heap.h"
 
-auto testBinaryHeapPushAndPop(const MinHeap<int>::ArrayType &array) {
+auto testBinaryHeap(const MinHeap<int>::ArrayType &array) {
     MinHeap<int> heap;
     for (const auto elem : array) {
         heap.Push(elem);
     }
+
+    heap.Delete(5); //Delete value 8
 
     MinHeap<int>::ArrayType sorted_array;
     while (not heap.Empty()) {
@@ -21,7 +23,7 @@ auto testBinaryHeapPushAndPop(const MinHeap<int>::ArrayType &array) {
 
 const MinHeap<int>::ArrayType SAMPLE_ARRAY {1, 0, 8, 6, 2, 3, 7, 4, 5, 9};
 const MinHeap<int>::ArrayType EXPECTED_ARRAY {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    0, 1, 2, 3, 4, 5, 6, 7, 9
 };
 
-SIMPLE_TEST(testBinaryHeapPushAndPop, TestSample, EXPECTED_ARRAY, SAMPLE_ARRAY);
+SIMPLE_TEST(testBinaryHeap, TestSample, EXPECTED_ARRAY, SAMPLE_ARRAY);
