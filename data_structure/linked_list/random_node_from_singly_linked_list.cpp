38d6c45a0cd2e196a3c534321e4112131c1414bd
Yang Yang <yang.yang@netscout.com>
2018-08-14 14:27:54 -0700

    LinkedList: Add random_node_from_singly_linked_list
    Change-Id: Ifb7708cb3bb1f16d04bca086a5bccce85a545395

---
+++ data_structure/linked_list/random_node_from_singly_linked_list.cpp
@@ -0,0 +1,23 @@
+#include "common_header.h"
+
+#include <forward_list>
+
+#include "mathematics/probabilistic/reservoir_sampling.h"
+
+using ArrayType = std::forward_list<int>;
+
+/** Select a Random Node from a Singly Linked List
+ *
+ * @reference   https://www.geeksforgeeks.org/select-a-random-node-from-a-singly-linked-list/
+ *
+ * Given a singly linked list, select a random node from linked list
+ * (the probability of picking a node should be 1/N if there are N
+ * nodes in list). You are given a random number generator.
+ */
+auto RandomNodeFromSinglyLinkedList(const ArrayType &stream) {
+    return ReservoirSampling(stream, 1).back();
+}
+
+
+ArrayType VALUES = {1, 2, 3, 4, 5, 6, 7, 8};
+SIMPLE_BENCHMARK(RandomNodeFromSinglyLinkedList, VALUES);
