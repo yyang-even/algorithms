0b4414c7d2dd50ec8b791680a9365a98a8d2a3eb
Yang Yang <yang.yang@netscout.com>
2018-08-14 14:27:54 -0700

    LinkedList: Add random_node_from_singly_linked_list
    Change-Id: Ifb7708cb3bb1f16d04bca086a5bccce85a545395

---
+++ mathematics/probabilistic/reservoir_sampling.h
@@ -0,0 +1,34 @@
+#include "common_header.h"
+
+/** Reservoir Sampling
+ *
+ * @reference   Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+ *              Introduction to Algorithms, Third Edition. Chapter 5.3. Exercises 5.3-7.
+ *
+ *              https://www.geeksforgeeks.org/reservoir-sampling/
+ *
+ * Reservoir sampling is a family of randomized algorithms for randomly choosing k samples
+ * from a list of n items, where n is either a very large or unknown number. Typically n
+ * is large enough that the list doesn’t fit into main memory. For example, a list of search
+ * queries in Google and Facebook.
+ *
+ * So we are given a big array (or stream) of numbers (to simplify), and we need to write
+ * an efficient function to randomly select k numbers where 1 <= k <= n.
+ * Let the input array be stream[].
+ *
+ * @complexity  O(n)
+ */
+template <typename Container>
+auto ReservoirSampling(const Container &stream, const typename Container::size_type k) {
+    assert(k < stream.size());
+
+    std::vector<typename Container::value_type> samples {stream.cbegin(), stream.cbegin() + k};
+    for (auto i = k, auto iter = stream.cbegin() + k; i < stream.size(); ++i, ++iter) {
+        const auto j = Random_Number<typename Container::size_type>(0, i);
+        if (j < k) {
+            samples[j] = *iter;
+        }
+    }
+
+    return samples;
+}
