65fc4237e5c18fbfe8f593d48ed3b49faca9b93b
Yang Yang <yang.yang@netscout.com>
2018-08-14 14:51:07 -0700

    Probabilistic: Add a variation of equal_probability_random_number
    Change-Id: Ie55f630a9a98f6b52e673275523781412513386b

--- mathematics/probabilistic/equal_probability_random_number.cpp
+++ mathematics/probabilistic/equal_probability_random_number.cpp
@@ -20,6 +20,27 @@ auto RandomNumber1to7() {
     return RandomNumber1to7();
 }
 
+
+/** Implement random-0-6-Generator using the given random-0-1-Generator
+ *
+ * @reference   https://www.geeksforgeeks.org/implement-random-0-6-generator-using-the-given-random-0-1-generator/
+ *
+ * Given a function random01Generator() that gives you randomly either 0 or 1,
+ * implement a function that utilizes this function and generate numbers between
+ * 0 and 6(both inclusive). All numbers should have same probabilities of occurrence.
+ */
+inline auto boo() {
+    return Random_Number(0, 1);
+}
+auto RandomNumber0to6() {
+    const auto i = boo() * 2 * 2 + boo() * 2 + boo();
+    if (i < 7) {
+        return i;
+    }
+    return RandomNumber0to6();
+}
+
+
 auto testRandomNumber1to7() {
     static const auto SAMPLE_SIZE = 7000000;
     static const auto AVERAGE_SAMPLE_SIZE = SAMPLE_SIZE / 7;
@@ -44,3 +65,29 @@ auto testRandomNumber1to7() {
 SIMPLE_BENCHMARK(RandomNumber1to7);
 
 SIMPLE_TEST0(testRandomNumber1to7, TestSample, true);
+
+
+auto testRandomNumber0to6() {
+    static const auto SAMPLE_SIZE = 7000000;
+    static const auto AVERAGE_SAMPLE_SIZE = SAMPLE_SIZE / 7;
+    static const double ERROR_RATE = 0.01;
+    static const int TOLERATION = AVERAGE_SAMPLE_SIZE * ERROR_RATE;
+
+    std::array<int, 7> counters {};
+    for (int i = 0; i < SAMPLE_SIZE; ++i) {
+        ++counters[RandomNumber0to6()];
+    }
+
+    for (auto iter = counters.cbegin(); iter != counters.cend(); ++iter) {
+        std::cout << *iter << std::endl;
+        if (AVERAGE_SAMPLE_SIZE - TOLERATION > *iter or AVERAGE_SAMPLE_SIZE + TOLERATION < *iter) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+SIMPLE_BENCHMARK(RandomNumber0to6);
+
+SIMPLE_TEST0(testRandomNumber0to6, TestSample, true);
