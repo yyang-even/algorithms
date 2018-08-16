bdc9631adcac3032dca362c954967ba73bb97572
Yang Yang <yang.yang@netscout.com>
2018-08-15 17:13:28 -0700

    Ordering: Add the external merge sort algorithm
    Change-Id: I80c7f574b2119a330ad0d5c65230444989c8f177

--- ordering/merge_sort.cpp
+++ ordering/merge_sort.cpp
@@ -91,6 +91,21 @@ auto MergeSortO1(ArrayType values) {
 }
 
 
+/** The external merge sort algorithm
+ *
+ * @reference   External Sorting
+ *              https://www.geeksforgeeks.org/external-sorting/
+ *
+ * External sorting is a term for a class of sorting algorithms that can handle massive
+ * amounts of data. External sorting is required when the data being sorted do not fit
+ * into the main memory of a computing device (usually RAM) and instead they must reside
+ * in the slower external memory (usually a hard drive). External sorting typically uses
+ * a hybrid sort-merge strategy. In the sorting phase, chunks of data small enough to fit
+ * in main memory are read, sorted, and written out to a temporary file. In the merge phase,
+ * the sorted sub-files are combined into a single larger file.
+ */
+
+
 const ArrayType VALUES1 = {};
 const ArrayType VALUES2 = {1};
 const ArrayType VALUES3 = {1, 2};
