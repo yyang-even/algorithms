a13191684fda3298f45022d96eeded3dedc638f2
Yang Yang <yyang.even@gmail.com>
2018-08-18 00:41:36 -0700

    Hash: Add check_all_elements_distinct
    Change-Id: Ie653a1672a2112ddfb2983694d4e1fbbaf4f715b

---
+++ hash/check_all_elements_distinct.cpp
@@ -0,0 +1,32 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+
+using ArrayType = std::vector<int>;
+/** Check if all array elements are distinct
+ *
+ * @reference   https://www.geeksforgeeks.org/check-if-all-array-elements-are-distinct/
+ *
+ * Given an array, check whether all elements in an array are distinct or not.
+ */
+auto CheckIfAllElementsDistinct(const ArrayType &elements) {
+    std::unordered_map<ArrayType::value_type, ArrayType::size_type> counters;
+
+    for (const auto elem : elements) {
+        if (++counters[elem] > 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+const ArrayType SAMPLE1 = {12, 10, 9, 45, 2, 10, 10, 45};
+const ArrayType SAMPLE2 = {2, 1, 9, 5};
+
+SIMPLE_BENCHMARK(CheckIfAllElementsDistinct, SAMPLE1);
+
+SIMPLE_TEST(CheckIfAllElementsDistinct, TestSAMPLE1, false, SAMPLE1);
+SIMPLE_TEST(CheckIfAllElementsDistinct, TestSAMPLE2, true, SAMPLE2);
