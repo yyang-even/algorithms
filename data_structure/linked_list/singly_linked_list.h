775c23ed03348975ee944de28d2d374295a13d70
Yang Yang <yyang.even@gmail.com>
2018-08-20 01:48:10 -0700

    Binary: Add check_only_first_and_last_bits_set
    Change-Id: I1b77d2cc4efab9bad59b9d29a84d30aabefffc56

--- data_structure/linked_list/singly_linked_list.h
+++ data_structure/linked_list/singly_linked_list.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "common_header.h"
 
 /** Singly Linked List
--- mathematics/numbers/binary/binary.h
+++ mathematics/numbers/binary/binary.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "common_header.h"
+
 unsigned CountSetBitsBrianKernighan(unsigned n);
+
+INT_BOOL IsPowerOf2(const unsigned num);
 
 unsigned LogBase2LookupTable(const unsigned num);
