9dee588ebfcae3c4ee010e273c757db523a16899
Yang Yang <yyang.even@gmail.com>
2018-08-21 15:38:37 -0700

    Binary: Add set_bit
    Change-Id: I916fc535c1117c920842296e0dc06890df027ce6

---
+++ mathematics/numbers/binary/set_bit.cpp
@@ -0,0 +1,19 @@
+#include "common_header.h"
+
+typedef unsigned InputType;
+
+/** How to set a bit in the number ‘num’
+ *
+ * @reference   Bitwise Hacks for Competitive Programming
+ *              https://www.geeksforgeeks.org/bitwise-hacks-for-competitive-programming/
+ */
+auto SetBit(const InputType num, const InputType position) {
+    return num | (1 << position);
+}
+
+
+SIMPLE_BENCHMARK(SetBit, 0, 4);
+
+SIMPLE_TEST(SetBit, TestSample1, 0b110, 0b100, 1);
+SIMPLE_TEST(SetBit, TestSample2, 1, 0, 0);
+SIMPLE_TEST(SetBit, TestSample3, 0b10, 0, 1);
