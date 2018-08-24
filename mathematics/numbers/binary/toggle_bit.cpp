29dd6ff9905cae495325150612860a85e5512a05
Yang Yang <yyang.even@gmail.com>
2018-08-23 23:02:37 -0700

    Binary: Add ToggleAllBitsExceptKth() to toggle_bit
    Change-Id: Iae9d035a871fe0fceca3ecf4623f0c21f8e3a1f6

--- mathematics/numbers/binary/toggle_bit.cpp
+++ mathematics/numbers/binary/toggle_bit.cpp
@@ -14,8 +14,22 @@ auto ToggleBit(const InputType num, const InputType position) {
 }
 
 
+/** Toggle all the bits of a number except k-th bit.
+ *
+ * @reference   https://www.geeksforgeeks.org/toggle-bits-number-except-k-th-bit/
+ */
+auto ToggleAllBitsExceptKth(const InputType num, const InputType position) {
+    return ~(num ^ (1 << position));
+}
+
+
 SIMPLE_BENCHMARK(ToggleBit, 0, 4);
 
 SIMPLE_TEST(ToggleBit, TestSample1, 0b110, 0b100, 1);
 SIMPLE_TEST(ToggleBit, TestSample2, 1, 0, 0);
 SIMPLE_TEST(ToggleBit, TestSample3, 0b10, 0, 1);
+
+
+SIMPLE_BENCHMARK(ToggleAllBitsExceptKth, 0, 4);
+
+SIMPLE_TEST(ToggleAllBitsExceptKth, TestSample1, 1, std::numeric_limits<InputType>::max(), 0);
