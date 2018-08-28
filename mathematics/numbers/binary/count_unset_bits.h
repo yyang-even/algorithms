385fc6b24c09676c406cbafeb5be99c2d4f5b2b3
Yang Yang <yyang.even@gmail.com>
2018-08-28 00:26:01 -0700

    Binary: Add count_equal_sum_and_xor
    Change-Id: I90ca1fd3d6050e7f6a593a1430e56a3b1f64163a

---
+++ mathematics/numbers/binary/count_unset_bits.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "toggle_all_bits_after_most_significant_bit.h"
+
+/** Count unset bits of a number
+ *
+ * @reference   https://www.geeksforgeeks.org/count-unset-bits-number/
+ *
+ * Given a number n, count unset bits after MSB (Most Significant Bit).
+ */
+#ifdef __GNUG__
+inline auto CountUnsetBits(const unsigned num) {
+    return __builtin_popcount(ToggleAllBitsAfterMSB(num));
+}
+#endif
