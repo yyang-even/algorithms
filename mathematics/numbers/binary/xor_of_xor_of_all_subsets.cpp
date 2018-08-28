18efda8ba74bc7b62bc5306bcdad8d67b8f83a8a
Yang Yang <yyang.even@gmail.com>
2018-08-28 01:02:51 -0700

    Binary: Add xor_of_xor_of_all_subsets
    Change-Id: I76ec25d4ec08a8fed5ec4a30c1eaca2771d7931f

---
+++ mathematics/numbers/binary/xor_of_xor_of_all_subsets.cpp
@@ -0,0 +1,30 @@
+#include "common_header.h"
+
+#include <unordered_set>
+
+#include "3rdParty/prettyprint.hpp"
+
+using SetType = std::unordered_set<unsigned>;
+
+/** Given a set, find XOR of the XOR’s of all subsets.
+ *
+ * @reference   https://www.geeksforgeeks.org/given-a-set-find-xor-of-the-xors-of-all-subsets/
+ * @reference   Bits manipulation (Important tactics)
+ *              https://www.geeksforgeeks.org/bits-manipulation-important-tactics/
+ *
+ * The question is to find XOR of the XOR’s of all subsets. i.e if the set is {1,2,3}.
+ * All subsets are : [{1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}]. Find the XOR
+ * of each of the subset and then find the XOR of every subset result.
+ */
+SetType::value_type FindXorOfXorOfAllSubsets(const SetType &s) {
+    return s.size() == 1 ? *s.cbegin() : 0u;
+}
+
+
+const SetType SAMPLE_SET = {1, 2, 3};
+const SetType SINGLE_SET = {5};
+
+SIMPLE_BENCHMARK(FindXorOfXorOfAllSubsets, SAMPLE_SET);
+
+SIMPLE_TEST(FindXorOfXorOfAllSubsets, TestSample1, 0, SAMPLE_SET);
+SIMPLE_TEST(FindXorOfXorOfAllSubsets, TestSample2, *SINGLE_SET.cbegin(), SINGLE_SET);
