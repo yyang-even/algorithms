e63761cd6251c573a6cf9110695146876516b7d4
Yang Yang <yyang.even@gmail.com>
2018-08-31 04:21:33 -0700

    Binary: Add find_the_odd_number
    Change-Id: I88d4883ffb4258555046c7bdba39693634b91e92

---
+++ mathematics/numbers/binary/find_the_odd_number.cpp
@@ -0,0 +1,32 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+using ArraryType = std::vector<unsigned>;
+
+/** Find the Number Occurring Odd Number of Times
+ *
+ * @reference   https://www.geeksforgeeks.org/find-the-number-occurring-odd-number-of-times/
+ *
+ * Given an array of positive integers. All numbers occur even number of times
+ * except one number which occurs odd number of times. Find the number in O(n)
+ * time & constant space.
+ *
+ * @complexity  O(n)
+ */
+auto FindTheOddNumber(const ArraryType &values) {
+    return std::accumulate(values.cbegin(), values.cend(), 0u, [](const auto & a, const auto & b) {
+        return a ^ b;
+    });
+}
+
+
+const ArraryType SAMPLE1 = {1, 2, 3, 2, 3, 1, 3};
+const ArraryType SAMPLE2 = {5, 7, 2, 7, 5, 2, 5};
+const ArraryType SAMPLE3 = {2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2};
+
+SIMPLE_BENCHMARK(FindTheOddNumber, SAMPLE1);
+
+SIMPLE_TEST(FindTheOddNumber, TestSample1, 3, SAMPLE1);
+SIMPLE_TEST(FindTheOddNumber, TestSample2, 5, SAMPLE2);
+SIMPLE_TEST(FindTheOddNumber, TestSample3, 5, SAMPLE3);
--- mathematics/numbers/binary/find_the_two_nonrepeating_elements.cpp
+++ mathematics/numbers/binary/find_the_two_nonrepeating_elements.cpp
@@ -16,6 +16,14 @@ using ArraryType = std::vector<unsigned>;
  *
  * @complexity  O(n)
  */
+/** Find the two numbers with odd occurrences in an unsorted array
+ *
+ * @reference   https://www.geeksforgeeks.org/find-the-two-numbers-with-odd-occurences-in-an-unsorted-array/
+ *
+ * Given an unsorted array that contains even number of occurrences for all
+ * numbers except two numbers. Find the two numbers which have odd occurrences
+ * in O(n) time complexity and O(1) extra space.
+ */
 auto FindTheTwoNonrepeatingElements(const ArraryType &elements) {
     ArraryType::value_type xor_of_all = 0u;
     ArraryType::value_type x = 0u;
@@ -39,9 +47,15 @@ auto FindTheTwoNonrepeatingElements(const ArraryType &elements) {
 }
 
 
-const ArraryType SAMPLE = {2, 4, 7, 9, 2, 4};
+const ArraryType SAMPLE1 = {2, 4, 7, 9, 2, 4};
+const ArraryType SAMPLE2 = {12, 23, 34, 12, 12, 23, 12, 45};
+const ArraryType SAMPLE3 = {4, 4, 100, 5000, 4, 4, 4, 4, 100, 100};
+const ArraryType SAMPLE4 = {10, 20};
 
 
-SIMPLE_BENCHMARK(FindTheTwoNonrepeatingElements, SAMPLE);
+SIMPLE_BENCHMARK(FindTheTwoNonrepeatingElements, SAMPLE1);
 
-SIMPLE_TEST(FindTheTwoNonrepeatingElements, TestSample1, std::make_pair(7u, 9u), SAMPLE);
+SIMPLE_TEST(FindTheTwoNonrepeatingElements, TestSample1, std::make_pair(7u, 9u), SAMPLE1);
+SIMPLE_TEST(FindTheTwoNonrepeatingElements, TestSample2, std::make_pair(45u, 34u), SAMPLE2);
+SIMPLE_TEST(FindTheTwoNonrepeatingElements, TestSample3, std::make_pair(100u, 5000u), SAMPLE3);
+SIMPLE_TEST(FindTheTwoNonrepeatingElements, TestSample4, std::make_pair(10u, 20u), SAMPLE4);
