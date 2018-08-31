ba238890b8e9d7287efe3061cfb91a3ab9cad29c
Yang Yang <yang.yang@netscout.com>
2018-08-31 16:05:20 -0700

    Binary: Add find_the_missing_number
    Change-Id: If0d4b21b096cb2221be224e58b51f7cd73e5f908

---
+++ mathematics/numbers/binary/find_the_missing_number.cpp
@@ -0,0 +1,53 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+using ArraryType = std::vector<unsigned>;
+
+/** Find the Missing Number
+ *
+ * @reference   https://www.geeksforgeeks.org/find-the-missing-number/
+ *
+ * You are given a list of n-1 integers and these integers are in the range of 1 to n.
+ * There are no duplicates in list. One of the integers is missing in the list.
+ * Write an efficient code to find the missing integer.
+ *
+ * @complexity  O(n)
+ */
+auto FindTheMissingNumberSum(const ArraryType &integers) {
+    assert(not integers.empty());
+
+    const auto N = integers.size() + 1u;
+    const auto sum = N * (N + 1) / 2;
+    return std::accumulate(integers.cbegin(), integers.cend(), sum, [](const auto & a, const auto & b) {
+        return a - b;
+    });
+}
+
+
+auto FindTheMissingNumberXor(const ArraryType &integers) {
+    assert(not integers.empty());
+
+    const auto N = integers.size() + 1u;
+    auto xor_of_all = integers[0];
+    for (ArraryType::size_type i = 1ul; i < integers.size(); ++i) {
+        xor_of_all ^= (i ^ integers[i]);
+    }
+
+    return xor_of_all ^ N ^ integers.size();
+}
+
+
+const ArraryType SAMPLE1 = {1, 2, 4, 6, 3, 7, 8};
+const ArraryType SAMPLE2 = {1, 2, 4, 5, 6};
+
+SIMPLE_BENCHMARK(FindTheMissingNumberSum, SAMPLE1);
+
+SIMPLE_TEST(FindTheMissingNumberSum, TestSample1, 5, SAMPLE1);
+SIMPLE_TEST(FindTheMissingNumberSum, TestSample2, 3, SAMPLE2);
+
+
+SIMPLE_BENCHMARK(FindTheMissingNumberXor, SAMPLE1);
+
+SIMPLE_TEST(FindTheMissingNumberXor, TestSample1, 5, SAMPLE1);
+SIMPLE_TEST(FindTheMissingNumberXor, TestSample2, 3, SAMPLE2);
