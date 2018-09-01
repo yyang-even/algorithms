a9fc429954b405441551bc4ff20d4185d8a81cf7
Yang Yang <yyang.even@gmail.com>
2018-09-01 01:51:16 -0700

    Binary: Add add_minimum_number_to_even_sum
    Change-Id: Idc24e7ab9db5f451fa321cc3b4b5eb8b923d2cf3

---
+++ mathematics/numbers/binary/add_minimum_number_to_even_sum.cpp
@@ -0,0 +1,35 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+using ArrayType = std::vector<unsigned>;
+
+/** Add minimum number to an array so that the sum becomes even
+ *
+ * @reference   https://www.geeksforgeeks.org/add-minimum-number-to-an-array-so-that-the-sum-becomes-even/
+ *
+ * Given an array, write a program to add the minimum number(should be greater than 0)
+ * to the array so that the sum of array becomes even.
+ *
+ * @complexity  O(n)
+ */
+auto AddMinimumNumberToEvenSum(const ArrayType &numbers) {
+    auto isOdd = false;
+
+    for (const auto num : numbers) {
+        if (num % 2) {
+            isOdd = !isOdd;
+        }
+    }
+
+    return isOdd ? 1u : 2u;
+}
+
+
+const ArrayType SAMPLE1 = {1, 2, 3, 4, 5, 6, 7, 8};
+const ArrayType SAMPLE2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+SIMPLE_BENCHMARK(AddMinimumNumberToEvenSum, SAMPLE1);
+
+SIMPLE_TEST(AddMinimumNumberToEvenSum, TestSample1, 2, SAMPLE1);
+SIMPLE_TEST(AddMinimumNumberToEvenSum, TestSample2, 1, SAMPLE2);
