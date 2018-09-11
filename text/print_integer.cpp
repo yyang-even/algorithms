0dc1f15b18692fd8cb5a09443521fc23436a96d1
Yang Yang <yang.yang@netscout.com>
2018-09-11 12:03:35 -0700

    Text: Add print_integer
    Change-Id: I5e9a32918176e4414f9b42e1f319a9d7a065f821

---
+++ text/print_integer.cpp
@@ -0,0 +1,47 @@
+#include "common_header.h"
+
+using InputType = int;
+
+/** Print a long int in C using putchar() only
+ *
+ * @reference   https://www.geeksforgeeks.org/print-long-int-number-c-using-putchar/
+ *
+ * Write a C function print(n) that takes a long int number n as argument, and prints it on console.
+ * The only allowed library function is putchar(), no other function like itoa() or printf() is
+ * allowed. Use of loops is also not allowed.
+ */
+void put_char(std::string &stream, const char c){
+    stream.push_back(c);
+}
+
+void PrintIntegerHelper(std::string &stream, const unsigned num) {
+    const auto next_n = num / 10u;
+    if (next_n)
+        PrintIntegerHelper(stream, next_n);
+
+    put_char(n % 10u + '0');
+}
+auto PrintInteger(InputType num) {
+    std::string stream;
+
+    if(num < 0){
+        put_char(stream, '-');
+        num = -num;
+}
+    
+    PrintIntegerHelper(stream, num);
+    return stream;
+}
+
+
+constexpr auto LOWER = std::numeric_limits<InputType>::min() + 1;
+constexpr auto UPPER = std::numeric_limits<InputType>::max();
+
+SIMPLE_BENCHMARK(PrintInteger, -1);
+
+SIMPLE_TEST(PrintInteger, TestSample1, std::string("-1"), -1);
+SIMPLE_TEST(PrintInteger, TestSample2, std::string("0"), 0);
+SIMPLE_TEST(PrintInteger, TestSample3, std::string("1"), 1);
+SIMPLE_TEST(PrintInteger, TestSample4, std::string("12045"), 12045);
+
+MUTUAL_RANDOM_TEST(std::to_string, PrintInteger, LOWER, UPPER);
