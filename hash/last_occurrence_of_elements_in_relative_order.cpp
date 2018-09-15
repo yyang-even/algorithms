7356ed2e6c327538e83fc6fded831d5fcadd76af
Yang Yang <yyang.even@gmail.com>
2018-09-15 02:45:13 -0700

    Hash: Add last_occurrence_of_elements_in_relative_order
    Change-Id: Iac748eb567a7c283c3c4d76687e583ece46e904c

---
+++ hash/last_occurrence_of_elements_in_relative_order.cpp
@@ -0,0 +1,40 @@
+#include "common_header.h"
+
+#include "3rdParty/prettyprint.hpp"
+
+using ArrayType = std::vector<int>;
+
+/** Print the last occurrence of elements in array in relative order
+ *
+ * @reference   https://www.geeksforgeeks.org/print-the-last-occurrence-of-elements-in-array-in-relative-order/
+ *
+ * Given an array of N elements, print the elements in the same relative order as
+ * given by removing all the occurrences of elements except the last occurrence.
+ */
+auto LastOccurrenceOfElementsInRelativeOrder(const ArrayType &elements) {
+    std::unordered_map<ArrayType::value_type, ArrayType::size_type> counters;
+
+    for (ArrayType::size_type i = 0; i < elements.size(); ++i) {
+        counters[elements[i]] = i;
+    }
+
+    ArrayType output;
+    for (ArrayType::size_type i = 0; i < elements.size(); ++i) {
+        if (i == counters[elements[i]]) {
+            output.push_back(elements[i]);
+        }
+    }
+
+    return output;
+}
+
+
+const ArrayType SAMPLE1 = {1, 5, 5, 1, 6, 1};
+const ArrayType EXPECTED1 = {5, 6, 1};
+const ArrayType SAMPLE2 = {2, 5, 5, 2};
+const ArrayType EXPECTED2 = {5, 2};
+
+SIMPLE_BENCHMARK(LastOccurrenceOfElementsInRelativeOrder, SAMPLE1);
+
+SIMPLE_TEST(LastOccurrenceOfElementsInRelativeOrder, TestSAMPLE1, EXPECTED1, SAMPLE1);
+SIMPLE_TEST(LastOccurrenceOfElementsInRelativeOrder, TestSAMPLE2, EXPECTED2, SAMPLE2);
