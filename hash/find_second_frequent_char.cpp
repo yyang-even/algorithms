e0c1312df611f6e281f0f2b45e16b5cad029bc8f
Yang Yang <yyang.even@gmail.com>
2018-09-21 01:33:23 -0700

    Hash: Add find_second_frequent_char
    Change-Id: Ief60ce2f53c1782ba4f64d73fc506091b529e5e8

---
+++ hash/find_second_frequent_char.cpp
@@ -0,0 +1,40 @@
+#include "common_header.h"
+
+/** Program to find second most frequent character
+ *
+ * @reference   https://www.geeksforgeeks.org/c-program-find-second-frequent-character/
+ *
+ * Given a string, find the second most frequent character in it. Expected
+ * time complexity is O(n) where n is the length of the input string.
+ */
+auto FindSecondFrequentChar(const std::string input) {
+    assert(input.size() > 1ul);
+
+    std::unordered_map<std::string::value_type, std::string::size_type> counters;
+
+    for (const auto c : input) {
+        ++counters[c];
+    }
+
+    std::string::size_type first_frequency = 0ul, second_frequency = 0ul;
+    std::string::value_type first, second;
+    for (const auto &elem : counters) {
+        if (elem.second > first_frequency) {
+            second_frequency = first_frequency;
+            second = first;
+            first_frequency = elem.second;
+            first = elem.first;
+        } else if (elem.second > second_frequency) {
+            second_frequency = elem.second;
+            second = elem.first;
+        }
+    }
+
+    return second;
+}
+
+
+SIMPLE_BENCHMARK(FindSecondFrequentChar, std::string("geeksforgeeks"));
+
+SIMPLE_TEST(FindSecondFrequentChar, TestSAMPLE1, 'g', std::string("geeksforgeeks"));
+SIMPLE_TEST(FindSecondFrequentChar, TestSAMPLE2, 'b', std::string("aabababa"));
