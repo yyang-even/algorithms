18eb73b95faf0e66dcfc730ed587eb3e0c5653e3
Yang Yang <yang.yang@netscout.com>
2018-09-25 16:32:08 -0700

    Ordering: Add another implementation to selection_sort
    Change-Id: I9145c0f7ad71281c0e7868985932f9be294e877f

--- ordering/selection_sort.cpp
+++ ordering/selection_sort.cpp
@@ -24,9 +24,23 @@ auto SelectionSort(Container values) {
                 smallest_iter = iter_j;
             }
         }
-        std::swap(*iter_i, *smallest_iter);
+        std::iter_swap(iter_i, smallest_iter);
     }
 
+    return values;
+}
+
+
+/**
+ * @reference   std::iter_swap
+ *              https://en.cppreference.com/w/cpp/algorithm/iter_swap
+ */
+template <typename Container>
+auto SelectionSortConcise(Container values) {
+    for(auto i = values.begin(); i != values.end(); ++i){
+        std::iter_swap(i, std::min_element(i, values.end()));
+    }
+    
     return values;
 }
 
@@ -39,11 +53,21 @@ constexpr ArrayType<3> EXPECTED4 = {1, 2, 3};
 constexpr ArrayType<4> VALUES5 = {4, 3, 2, 1};
 constexpr ArrayType<4> EXPECTED5 = {1, 2, 3, 4};
 
+SIMPLE_BENCHMARK(SelectionSort, VALUES5);
+
 SIMPLE_TEST(SelectionSort, TestSAMPLE1, VALUES1, VALUES1);
 SIMPLE_TEST(SelectionSort, TestSAMPLE2, VALUES2, VALUES2);
 SIMPLE_TEST(SelectionSort, TestSAMPLE3, VALUES3, VALUES3);
 SIMPLE_TEST(SelectionSort, TestSAMPLE4, EXPECTED4, VALUES4);
 SIMPLE_TEST(SelectionSort, TestSAMPLE5, EXPECTED5, VALUES5);
+
+SIMPLE_BENCHMARK(SelectionSortConcise, VALUES5);
+
+SIMPLE_TEST(SelectionSortConcise, TestSAMPLE1, VALUES1, VALUES1);
+SIMPLE_TEST(SelectionSortConcise, TestSAMPLE2, VALUES2, VALUES2);
+SIMPLE_TEST(SelectionSortConcise, TestSAMPLE3, VALUES3, VALUES3);
+SIMPLE_TEST(SelectionSortConcise, TestSAMPLE4, EXPECTED4, VALUES4);
+SIMPLE_TEST(SelectionSortConcise, TestSAMPLE5, EXPECTED5, VALUES5);
 
 /** C program to sort an array of strings using Selection Sort
  *
@@ -55,6 +79,8 @@ const std::vector<std::string> VALUES23 = {"paper", "true", "soap", "floppy", "f
 const std::vector<std::string> EXPECTED23 = {"floppy", "flower", "paper", "soap", "true"};
 const std::vector<std::string> VALUES24 = {"paper", "soap", "floppy", "flower"};
 const std::vector<std::string> EXPECTED24 = {"floppy", "flower", "paper", "soap"};
+
+SIMPLE_BENCHMARK(SelectionSort, VALUES24);
 
 SIMPLE_TEST(SelectionSort, TestSAMPLE21, VALUES21, VALUES21);
 SIMPLE_TEST(SelectionSort, TestSAMPLE22, VALUES22, VALUES22);
