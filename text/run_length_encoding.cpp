aecabc80761dad98a83ed1d80ddf3ef4d370e1bd
Yang Yang <yang.yang@netscout.com>
2018-09-25 14:00:54 -0700

    Text: Add run_length_encoding
    Change-Id: I1d4453d960720f6811ada3d5f5d7c15f374611fc

---
+++ text/run_length_encoding.cpp
@@ -0,0 +1,30 @@
+#include "common_header.h"
+
+/** Run Length Encoding
+ *
+ * @reference   https://www.geeksforgeeks.org/run-length-encoding/
+ *
+ * Given an input string, write a function that returns the Run Length Encoded string for the input string.
+ * For example, if the input string is “wwwwaaadexxxxxx”, then the function should return “w4a3d1e1x6”.
+ */
+auto RunLengthEncoding(const std::string &input) {
+    std::string output;
+
+    for (std::string::size_type i = 0; i < input.size();) {
+        auto next_i = input.find_first_not_of(input[i], i);
+        if (next_i == std::string::npos) {
+            next_i = input.size();
+        }
+        const auto count = next_i - i;
+        output.push_back(input[i]);
+        output.append(std::to_string(count));
+        i = next_i;
+    }
+
+    return output;
+}
+
+
+SIMPLE_BENCHMARK(RunLengthEncoding, std::string("wwwwaaadexxxxxx"));
+
+SIMPLE_TEST(RunLengthEncoding, TestSAMPLE1, "w4a3d1e1x6", std::string("wwwwaaadexxxxxx"));
