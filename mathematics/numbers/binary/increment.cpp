19211910d0763715e69836f0207eea0d50a480df
Yang Yang <yyang.even@gmail.com>
2018-09-25 02:51:44 -0700

    Binary: Add increment
    Change-Id: I1d87c323aea95d99ef18e0d84721b4a2fd6cd3b2

---
+++ mathematics/numbers/binary/increment.cpp
@@ -0,0 +1,19 @@
+#include "common_header.h"
+
+using InputType = unsigned;
+
+/** Increment a number without using ++ or +
+ *
+ * @reference   https://www.geeksforgeeks.org/increment-a-number-without-using-or/
+ *
+ * The task is to Increment a number without using ++ and + operators.
+ */
+auto Increment(const auto num) {
+    return -(~num);
+}
+
+
+SIMPLE_BENCHMARK(Increment, 4);
+
+SIMPLE_TEST(Increment, TestSample1, 4, 3);
+SIMPLE_TEST(Increment, TestSample2, 10, 9);
