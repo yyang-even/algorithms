eeed5e25cd49469b3800f3f830485adcaf7e1e72
Yang Yang <yang.yang@netscout.com>
2018-09-25 18:10:00 -0700

    Text: Add count_chars_at_same_position_as_in_alphabet
    Change-Id: I0b5aa79761c92a877e84d5f499dc0feaa10d234a

---
+++ text/count_chars_at_same_position_as_in_alphabet.cpp
@@ -0,0 +1,28 @@
+#include "common_header.h"
+
+/** Count characters at same position as in English alphabet
+ *
+ * @reference   https://www.geeksforgeeks.org/count-characters-at-same-position-as-in-english-alphabet/
+ *
+ * Given a string of lower and uppercase characters, the task is to find
+ * that how many characters are at same position as in English alphabet.
+ */
+auto CountCharsAtSamePositionAsInAlphabet(const std::string &input) {
+    std::string::size_type count = 0ul;
+
+    for (std::string::size_type i = 0; i < input.size(); ++i) {
+        if (i == input[i] - 'a' or i == input[i] - 'A') {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+
+SIMPLE_BENCHMARK(CountCharsAtSamePositionAsInAlphabet, std::string("geeksforgeeks"));
+
+SIMPLE_TEST(CountCharsAtSamePositionAsInAlphabet, TestSAMPLE1, 3ul, std::string("ABcED"));
+SIMPLE_TEST(CountCharsAtSamePositionAsInAlphabet, TestSAMPLE2, 1ul, std::string("geeksforgeeks"));
+SIMPLE_TEST(CountCharsAtSamePositionAsInAlphabet, TestSAMPLE3, 3ul, std::string("alphabetical"));
+SIMPLE_TEST(CountCharsAtSamePositionAsInAlphabet, TestSAMPLE4, 5ul, std::string("AbgdeF"));
