384b1b77e7c773d3dac12df691a4e9a02d8bfb12
Yang Yang <yyang.even@gmail.com>
2018-09-27 23:56:58 -0700

    Binary: Add rotate_bits
    Change-Id: I0a5f2319ed2ca06f42420775fcd75e6ed46dbe0b

---
+++ mathematics/numbers/binary/rotate_bits.cpp
@@ -0,0 +1,31 @@
+#include "common_header.h"
+
+using InputType = unsigned;
+
+/** Rotate bits of a number
+ *
+ * @reference   https://www.geeksforgeeks.org/rotate-bits-of-an-integer/
+ *
+ * Bit Rotation: A rotation (or circular shift) is an operation similar to shift
+ * except that the bits that fall off at one end are put back to the other end.
+ * In left rotation, the bits that fall off at left end are put back at right end.
+ * In right rotation, the bits that fall off at right end are put back at left end.
+ */
+auto LeftRotate(const InputType num, const InputType r) {
+    return (num << r) | (num >> (Bits_Number<decltype(num)>() - r));
+}
+
+auto RightRotate(const InputType num, const InputType r) {
+    return (num >> r) | (num << (Bits_Number<decltype(num)>() - r));
+}
+
+
+SIMPLE_BENCHMARK(LeftRotate, 0b1101, 4);
+
+SIMPLE_TEST(LeftRotate, TestSample1, 0b1000, 1, 3);
+SIMPLE_TEST(LeftRotate, TestSample2, 64, 16, 2);
+
+SIMPLE_BENCHMARK(RightRotate, 0b1101, 4);
+
+SIMPLE_TEST(RightRotate, TestSample1, 1u, 0b1000, 3);
+SIMPLE_TEST(RightRotate, TestSample2, 4u, 16, 2);
