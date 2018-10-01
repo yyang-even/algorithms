2c06b0a210475fdfe297f0f9d9acebaf19d3bda6
Yang Yang <yang.yang@netscout.com>
2018-10-01 15:36:24 -0700

    Text: Add count_strings_with_consonant_and_vowels_at_alternate_position
    Change-Id: I13eeab0b0783ed51f879ee5012cd36c5e0eb15a1

---
+++ text/count_strings_with_consonant_and_vowels_at_alternate_position.cpp
@@ -0,0 +1,30 @@
+#include "common_header.h"
+
+/** Count strings with consonants and vowels at alternate position
+ *
+ * @reference   Given a string str. The task is to find all possible number of strings
+ * that can be obtained by replacing the "$" with alphabets in the given string.
+ * Note: Alphabets should be placed in such a way that the string is always alternating
+ * in vowels and consonants, and the string must always start with a consonant. It is
+ * assumed that such a string is always possible, i.e. there is no need to care about
+ * the characters other than “$”.
+ */
+auto CountStringsWithConsonantAndVowelsAtAlternatePosition(const std::string str) {
+    std::string::size_type total = 1ul;
+
+    for (std::string::size_type i = 0ul; i < str.size(); ++i) {
+        if (str[i] == '$') {
+            total *= (i % 2 ? 5ul : 21ul);
+        }
+    }
+
+    return total;
+}
+
+
+SIMPLE_BENCHMARK(CountStringsWithConsonantAndVowelsAtAlternatePosition, std::string("s$$e$"));
+
+SIMPLE_TEST(CountStringsWithConsonantAndVowelsAtAlternatePosition, TestSAMPLE1, 2205ul,
+            std::string("s$$e$"));
+SIMPLE_TEST(CountStringsWithConsonantAndVowelsAtAlternatePosition, TestSAMPLE2, 5ul,
+            std::string("y$s"));
