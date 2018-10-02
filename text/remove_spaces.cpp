306013e23542a942d2d356027666cb87c0874e44
Yang Yang <yang.yang@netscout.com>
2018-10-02 12:58:15 -0700

    Text: Add remove_spaces
    Change-Id: I7eecb817bcce80d678f798279f70d33acc255cbb

---
+++ text/remove_spaces.cpp
@@ -0,0 +1,26 @@
+#include "common_header.h"
+
+/** Remove spaces from a given string
+ *
+ * @reference   https://www.geeksforgeeks.org/remove-spaces-from-a-given-string/
+ *
+ * Given a string, remove all spaces from the string and return it.
+ * Expected time complexity is O(n) and only one traversal of string.
+ */
+auto RemoveSpaces(std::string input) {
+    auto output_iter = input.begin();
+    for (auto input_iter = input.begin(); input_iter != input.end(); ++input_iter) {
+        if (*input_iter != ' ') {
+            *output_iter++ = *input_iter;
+        }
+    }
+
+    input.resize(output_iter - input.begin());
+    return input;
+}
+
+
+SIMPLE_BENCHMARK(RemoveSpaces, std::string("g  eeks   for ge  eeks  "));
+
+SIMPLE_TEST(RemoveSpaces, TestSAMPLE1, std::string("geeksforgeeeks"),
+            std::string("g  eeks   for ge  eeks  "));
