1dacebcdb1042da3e2eaba9ed35e1176da644d9f
Yang Yang <yang.yang@netscout.com>
2018-10-11 12:32:56 -0700

    Multiplication: Add multiplication
    Change-Id: I3548123365d381c6637703df5633bef5c5448e68

---
+++ mathematics/arithmetics/multiplication/multiplication.cpp
@@ -0,0 +1,29 @@
+#include "common_header.h"
+
+using ArrayType = std::vector<int>;
+
+/** Program for multiplication of array elements
+ *
+ * @reference   https://www.geeksforgeeks.org/program-multiplication-array-elements/
+ *
+ * We are given an array and we have to calculate the product of an array using both iterative and recursive method. 
+ */
+auto MultiplicationRecersive(const ArrayType::const_iterator cbegin, const ArrayType::const_iterator cend){
+    if(cbegin == cend)
+        return 1ll;
+    else
+        return MultiplicationRecersive(cbegin + 1, cend) * (*cbegin);
+}
+auto MultiplicationRecersive(const ArrayType &elements){
+    return MultiplicationRecersive(elements.cbegin(), elements.cend());
+}
+
+
+const ArrayType SAMPLE1 = {1, 2, 3, 4, 5, 6};
+const ArrayType SAMPLE2 = {1, 3, 5, 7, 9};
+
+
+SIMPLE_BENCHMARK(MultiplicationRecersive, SAMPLE1);
+
+SIMPLE_TEST(MultiplicationRecersive, TestSAMPLE1, 720, SAMPLE1);
+SIMPLE_TEST(MultiplicationRecersive, TestSAMPLE2, 945, SAMPLE2);
