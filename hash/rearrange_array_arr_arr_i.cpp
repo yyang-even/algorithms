515654308da53dba8ec4e2fb55e0f7f6c0d6e80c
Yang Yang <yang.yang@netscout.com>
2018-10-11 14:28:24 -0700

    Hash: Add rearrange_array_arr_arr_i
    Change-Id: I94266f5c3888fa891941ad6ab3959170eeaebe2a

---
+++ hash/rearrange_array_arr_arr_i.cpp
@@ -0,0 +1,40 @@
+#include "common_header.h"
+
+using ArrayType = std::vector<unsigned>;
+
+/** Rearrange an array so that arr[i] becomes arr[arr[i]] with O(1) extra space
+ *
+ * @reference   https://www.geeksforgeeks.org/rearrange-given-array-place/
+ *
+ * Given an array arr[] of size n where every element is in range from 0 to n-1.
+ * Rearrange the given array so that arr[i] becomes arr[arr[i]]. This should be
+ * done with O(1) extra space.
+ */
+auto RearrangeArrayInPlace(ArrayType values) {
+    std::transform(values.cbegin(), values.cend(),
+    values.begin(), [&values](const ArrayType::value_type v) {
+        return v + values[v] % values.size() * values.size();
+    });
+
+    std::transform(values.cbegin(), values.cend(),
+    values.begin(), [&values](const ArrayType::value_type v) {
+        return v / values.size();
+    });
+
+    return values;
+}
+
+
+const ArrayType SAMPLE1 = {3, 2, 0, 1};
+const ArrayType EXPECTED1 = {1, 0, 3, 2};
+const ArrayType SAMPLE2 = {4, 0, 2, 1, 3};
+const ArrayType EXPECTED2 = {3, 4, 2, 0, 1};
+const ArrayType SAMPLE3 = {0, 1, 2, 3};
+const ArrayType EXPECTED3 = {0, 1, 2, 3};
+
+
+SIMPLE_BENCHMARK(RearrangeArrayInPlace, SAMPLE1);
+
+SIMPLE_TEST(RearrangeArrayInPlace, TestSAMPLE1, EXPECTED1, SAMPLE1);
+SIMPLE_TEST(RearrangeArrayInPlace, TestSAMPLE2, EXPECTED2, SAMPLE2);
+SIMPLE_TEST(RearrangeArrayInPlace, TestSAMPLE3, EXPECTED3, SAMPLE3);
