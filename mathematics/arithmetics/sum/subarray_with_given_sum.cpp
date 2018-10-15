9d5fffe0beab00e4e9e1d686b57b1d8ce407f0c5
Yang Yang <yyang.even@gmail.com>
2018-10-15 01:53:48 -0700

    Arithmetics: Add another method for subarray_with_given_sum
    Change-Id: I8a9f41f1f421d3c1aa3432b9418b435b68a5e659

--- mathematics/arithmetics/sum/subarray_with_given_sum.cpp
+++ mathematics/arithmetics/sum/subarray_with_given_sum.cpp
@@ -30,6 +30,34 @@ std::pair<int, int> FindSubarrayWithGivenSum(const ArrayType &integers,
     return std::make_pair(-1, -1);
 }
 
+/** Find subarray with given sum | Set 2 (Handles Negative Numbers)
+ *
+ * @reference   https://www.geeksforgeeks.org/find-subarray-with-given-sum-in-array-of-integers/
+ */
+std::pair<int, int> FindSubarrayWithGivenSumMap(const ArrayType &integers,
+        const ArrayType::value_type SUM) {
+    std::unordered_map<ArrayType::value_type, ArrayType::size_type> sum_index_map;
+    ArrayType::value_type current_sum = 0;
+
+    for (ArrayType::size_type i = 0ul; i < integers.size(); ++i) {
+        current_sum += integers[i];
+
+        if (current_sum == SUM) {
+            return std::make_pair(0, i);
+        }
+
+        const auto expected_subarray_sum = current_sum - SUM;
+        const auto iter = sum_index_map.find(expected_subarray_sum);
+        if (iter != sum_index_map.cend()) {
+            return std::make_pair(iter->second + 1, i);
+        }
+
+        sum_index_map[current_sum] = i;
+    }
+
+    return std::make_pair(-1, -1);
+}
+
 
 const ArrayType SAMPLE1 = {1, 4, 20, 3, 10, 5};
 const ArrayType SAMPLE2 = {1, 4, 0, 0, 3, 10, 5};
@@ -43,3 +71,16 @@ SIMPLE_TEST(FindSubarrayWithGivenSum, TestSample2, std::make_pair(1, 4), SAMPLE2
 SIMPLE_TEST(FindSubarrayWithGivenSum, TestSample3, std::make_pair(-1, -1), SAMPLE3, 0);
 SIMPLE_TEST(FindSubarrayWithGivenSum, TestSample4, std::make_pair(1, 4), SAMPLE4, 23);
 SIMPLE_TEST(FindSubarrayWithGivenSum, TestSample5, std::make_pair(3, 3), SAMPLE4, 8);
+
+const ArrayType SAMPLE6 = {10, 2, -2, -20, 10};
+const ArrayType SAMPLE7 = { -10, 0, 2, -2, -20, 10};
+
+SIMPLE_BENCHMARK(FindSubarrayWithGivenSumMap, SAMPLE1, 33);
+
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample1, std::make_pair(2, 4), SAMPLE1, 33);
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample2, std::make_pair(1, 4), SAMPLE2, 7);
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample3, std::make_pair(-1, -1), SAMPLE3, 0);
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample4, std::make_pair(1, 4), SAMPLE4, 23);
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample5, std::make_pair(3, 3), SAMPLE4, 8);
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample6, std::make_pair(0, 3), SAMPLE6, -10);
+SIMPLE_TEST(FindSubarrayWithGivenSumMap, TestSample7, std::make_pair(-1, -1), SAMPLE7, 20);
