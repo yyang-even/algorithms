ac7f6ddfcde1a84f9ff833412927d9844e58aad9
Yang Yang <yang.yang@netscout.com>
2018-10-17 13:55:01 -0700

    Difference: Add minimum_difference_between_two_elements
    Change-Id: I6b60a6f1532152b623dd5b60997167954470cfc9

---
+++ mathematics/arithmetics/difference/minimum_difference_between_two_elements.cpp
@@ -0,0 +1,35 @@
+#include "common_header.h"
+
+using ArrayType = std::vector<int>;
+/** Find minimum difference between any two elements
+ *
+ * @reference   https://www.geeksforgeeks.org/find-minimum-difference-pair/
+ *
+ * Given an unsorted array, find the minimum difference between any pair in given array.
+ */
+auto FindMinDifferenceBetweenTwoElements(ArrayType elements){
+    assert(elements.size() > 1);
+
+    std::sort(elements.begin(), elements.end());
+
+    auto min_diff = std::numeric_limits<ArrayType::value_type>::max();
+    for(ArrayType::size_type i = 1; i < elements.size(); ++i) {
+        const auto diff = elements[i] - elements[i-1];
+        if (diff < min_diff)
+            min_diff = diff;
+    }
+
+    return min_diff;
+}
+
+
+const ArrayType SAMPLE1 = {1, 5, 3, 19, 18, 25};
+const ArrayType SAMPLE2 = {30, 5, 20, 9};
+const ArrayType SAMPLE3 = {1, 19, -4, 31, 38, 25, 100};
+
+
+SIMPLE_BENCHMARK(FindMinDifferenceBetweenTwoElements, SAMPLE1);
+
+SIMPLE_TEST(FindMinDifferenceBetweenTwoElements, TestSample1, 1, SAMPLE1);
+SIMPLE_TEST(FindMinDifferenceBetweenTwoElements, TestSample2, 4, SAMPLE2);
+SIMPLE_TEST(FindMinDifferenceBetweenTwoElements, TestSample3, 5, SAMPLE3);
