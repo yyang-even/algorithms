ac7db18d7e37ed1d1ad690898304d8a1346a0b90
Yang Yang <yyang.even@gmail.com>
2018-11-30 05:27:27 -0800

    Hash: Add a_pair_primes_product_as_n
    Change-Id: I727d2cd5a6d584f5bd0c5384642b7a897a09d647

---
+++ hash/a_pair_elements_product_as_n.h
@@ -0,0 +1,29 @@
+#pragma once
+
+template<typename ArrayType>
+auto PairElementsProductAsN(const ArrayType &elements, const typename ArrayType::value_type N,
+                            std::pair<typename ArrayType::value_type, typename ArrayType::value_type> *output) {
+    std::unordered_set<typename ArrayType::value_type> value_set{};
+
+    for (const auto elem : elements) {
+        if (elem == 0 and N == 0) {
+            if (output) {
+                *output = std::make_pair(elem, elem);
+            }
+            return true;
+        }
+
+        if (N % elem == 0) {
+            const auto pair = N / elem;
+            if (value_set.find(pair) != value_set.cend()) {
+                if (output) {
+                    *output = std::make_pair(pair, elem);
+                }
+                return true;
+            }
+            value_set.insert(elem);
+        }
+    }
+
+    return false;
+}
