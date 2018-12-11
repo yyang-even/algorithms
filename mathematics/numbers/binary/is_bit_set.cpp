7af1aabb8bb8e6c1d1933bf743ca0df6446b4d70
Yang Yang <yyang.even@gmail.com>
2018-12-11 00:10:30 -0800

    Binary: Add a variation of is_bit_set
    Change-Id: Ice39d7acc068136469eecad941fe6ce0cc1f63ad

--- mathematics/numbers/binary/is_bit_set.cpp
+++ mathematics/numbers/binary/is_bit_set.cpp
@@ -8,10 +8,10 @@ using InputType = unsigned;
  *              https://www.geeksforgeeks.org/bitwise-hacks-for-competitive-programming/
  * @reference   Check whether K-th bit is set or not
  *              https://www.geeksforgeeks.org/check-whether-k-th-bit-set-not/
- */
-/** Print ‘K’th least significant bit of a number
- *
- * @reference   https://www.geeksforgeeks.org/print-kth-least-significant-bit-number/
+ * @reference   Print ‘K’th least significant bit of a number
+ *              https://www.geeksforgeeks.org/print-kth-least-significant-bit-number/
+ * @reference   Find value of k-th bit in binary representation
+ *              https://www.geeksforgeeks.org/find-value-k-th-bit-binary-representation/
  *
  * A number N is given. We need to print its ‘K’th Least Significant Bit.
  */
@@ -25,3 +25,5 @@ SIMPLE_BENCHMARK(IsBitSet, 0, 4);
 SIMPLE_TEST(IsBitSet, TestSample1, TRUE, 0b101, 0);
 SIMPLE_TEST(IsBitSet, TestSample2, FALSE, 0, 0);
 SIMPLE_TEST(IsBitSet, TestSample3, FALSE, 0, 1);
+SIMPLE_TEST(IsBitSet, TestSample4, FALSE, 13, 1);
+SIMPLE_TEST(IsBitSet, TestSample5, TRUE, 14, 2);
