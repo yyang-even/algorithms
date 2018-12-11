0177d1a77e55c72e1fc8a1964f36ba295ec1a5bc
Yang Yang <yyang.even@gmail.com>
2018-12-11 00:03:06 -0800

    Binary: Add a variation of count_diff_bits_of_two
    Change-Id: I2ec50979bccfa5a43b3cfd99ce4ed99eae7aefec

--- mathematics/numbers/binary/count_diff_bits_of_two.cpp
+++ mathematics/numbers/binary/count_diff_bits_of_two.cpp
@@ -7,6 +7,8 @@ typedef unsigned InputType;
 /** Count number of bits to be flipped to convert A to B
  *
  * @reference   https://www.geeksforgeeks.org/count-number-of-bits-to-be-flipped-to-convert-a-to-b/
+ * @reference   Number of mismatching bits in the binary representation of two integers
+ *              https://www.geeksforgeeks.org/number-of-mismatching-bits-in-the-binary-representation-of-two-integers/
  *
  * Given two numbers ‘a’ and b’. Write a program to count number of bits needed to be flipped to convert ‘a’ to ‘b’.
  */
@@ -19,3 +21,5 @@ SIMPLE_BENCHMARK(CountDiffBits, 10, 20);
 
 SIMPLE_TEST(CountDiffBits, TestSample1, 4, 10, 20);
 SIMPLE_TEST(CountDiffBits, TestSample2, 3, 10, 7);
+SIMPLE_TEST(CountDiffBits, TestSample3, 2, 12, 15);
+SIMPLE_TEST(CountDiffBits, TestSample4, 3, 3, 16);
