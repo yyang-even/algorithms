0d458f68fc9b6616f95403ce11351af9bd3ab124
Yang Yang <yyang.even@gmail.com>
2019-01-14 16:00:27 -0800

    Prime: Add least_prime_factor_of_numbers_till_n
    Change-Id: I00626bcd454f880807200911eebf849cd481e406

---
+++ mathematics/numbers/prime/least_prime_factor_of_numbers_till_n.cpp
@@ -0,0 +1,38 @@
+#include "common_header.h"
+
+/** Least prime factor of numbers till n
+ *
+ * @reference   https://www.geeksforgeeks.org/least-prime-factor-of-numbers-till-n/
+ *
+ * Given a number n, print least prime factors of all numbers from 1 to n. The least
+ * prime factor of an integer n is the smallest prime number that divides the number.
+ * The least prime factor of all even numbers is 2. A prime number is its own least
+ * prime factor (as well as its own greatest prime factor).
+ * Note: We need to print 1 for 1.
+ */
+auto LeastPrimeFactorOfNumbers(const unsigned N) {
+    assert(N > 1);
+    std::vector<unsigned> smallest_prime_factors(N + 1, 0);
+    smallest_prime_factors[1] = 1;
+
+    unsigned j;
+    for (unsigned i = 2; i <= N; ++i) {
+        if (smallest_prime_factors[i] == 0) {
+            smallest_prime_factors[i] = i;
+            for (j = i * 2; j <= N; j += i) {
+                if (smallest_prime_factors[j] == 0) {
+                    smallest_prime_factors[j] = i;
+                }
+            }
+        }
+    }
+
+    return smallest_prime_factors;
+}
+
+
+const std::vector<unsigned> RESULT1 = {0, 1, 2, 3, 2, 5, 2, 7, 2, 3, 2};
+
+SIMPLE_BENCHMARK(LeastPrimeFactorOfNumbers, 10);
+
+SIMPLE_TEST(LeastPrimeFactorOfNumbers, TestSample1, RESULT1, 10);
