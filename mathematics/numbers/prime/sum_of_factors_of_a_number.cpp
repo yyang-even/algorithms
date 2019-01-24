7591c60185d986b1ad429a62c68f40a82e6c8159
Yang Yang <yyang.even@gmail.com>
2019-01-23 16:36:42 -0800

    Prime: Add sum_of_factors_of_a_number
    Change-Id: I7d72a1c6eb5afb8dc9c1f14de487e94bb679cc09

---
+++ mathematics/numbers/prime/sum_of_factors_of_a_number.cpp
@@ -0,0 +1,37 @@
+#include "common_header.h"
+
+#include "prime_factors_of_n.h"
+
+/** Sum of Factors of a Number using Prime Factorization
+ *
+ * @reference   https://www.geeksforgeeks.org/sum-of-factors-of-a-number-using-prime-factorization/
+ *
+ * Given a number N. The task is to find the sum of all factors of the given number N.
+ */
+auto SumOfFactorsOfN(const unsigned N) {
+    const auto prime_factors = PrimeFactorsOf(N);
+
+    unsigned power = 1;
+    unsigned current = 1;
+    unsigned sub_sum = 1;
+    unsigned sum = 1;
+    for (const auto prime : prime_factors) {
+        if (prime != current) {
+            sum *= sub_sum;
+            current = prime;
+            sub_sum = 1;
+            power = 1;
+        }
+        power *= prime;
+        sub_sum += power;
+    }
+
+    return sum * sub_sum;
+}
+
+
+SIMPLE_BENCHMARK(SumOfFactorsOfN, 1100);
+
+SIMPLE_TEST(SumOfFactorsOfN, TestSAMPLE1, 28, 12);
+SIMPLE_TEST(SumOfFactorsOfN, TestSAMPLE2, 168, 60);
+SIMPLE_TEST(SumOfFactorsOfN, TestSAMPLE3, 2604, 1100);
