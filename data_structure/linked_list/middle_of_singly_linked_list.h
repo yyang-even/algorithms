cb1314604e4708037384916125ddfdaa59b8f7a2
Yang Yang <yyang.even@gmail.com>
2019-02-15 16:06:44 -0800

    LinkedList: Add more solutions to middle_of_singly_linked_list
    Change-Id: I56ea3bba83ef7c8c61786fef74949d1d811d4677

---
+++ data_structure/linked_list/middle_of_singly_linked_list.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <forward_list>
+
+
+auto GetBeforeMiddleTwoPointersSTL(const std::forward_list<int>::const_iterator cbefore_begin,
+                                   const std::forward_list<int>::const_iterator cend) {
+    auto slow_ptr = cbefore_begin;
+    auto fast_ptr = std::next(cbefore_begin);
+
+    assert(fast_ptr != cend);
+
+    for (; fast_ptr != cend and ++fast_ptr != cend; ++fast_ptr, ++slow_ptr);
+    return slow_ptr;
+}
