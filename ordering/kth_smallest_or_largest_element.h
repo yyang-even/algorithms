b82cf1cac5dab2a3cc077b5e640ecb744d26c401
Yang Yang <yyang.even@gmail.com>
2019-03-20 16:31:51 -0700

    Ordering: Add kth_smallest_or_largest_element
    Change-Id: I07b96b01924e97a58191166c838ffe2e5dc5c324

---
+++ ordering/kth_smallest_or_largest_element.h
@@ -0,0 +1,18 @@
+#pragma once
+
+auto KthSmallest_QuickSelect(const ArrayType::iterator begin, const ArrayType::iterator end,
+                             const ArrayType::const_iterator target) {
+    const auto pivot = std::prev(end);
+    const auto partition_point = std::partition(begin, pivot, [pivot](const auto v) {
+        return v <= *pivot;
+    });
+    std::iter_swap(pivot, partition_point);
+
+    if (target == partition_point) {
+        return partition_point;
+    } else if (partition_point > target) {
+        return KthSmallest_QuickSelect(begin, partition_point, target);
+    } else {
+        return KthSmallest_QuickSelect(std::next(partition_point), end, target);
+    }
+}
