1e6c263d8cc98425598efdc1f7163aa735d7164d
Yang Yang <yyang.even@gmail.com>
2019-05-23 10:12:03 -0700

    Subtract: Add another method for subtract_1
    Change-Id: I297c18e015375160f23c4f38a452d223cd1267bb

--- mathematics/arithmetics/subtract/subtract_1.cpp
+++ mathematics/arithmetics/subtract/subtract_1.cpp
@@ -1,5 +1,7 @@
 #include "common_header.h"
 
+namespace {
+
 /** Subtract 1 without arithmetic operators
  *
  * @reference   https://www.geeksforgeeks.org/subtract-1-without-arithmetic-operators/
@@ -7,18 +9,32 @@
  * Write a program to subtract one from a given number. The use of operators
  * like ‘+’, ‘-‘, ‘*’, ‘/’, ‘++’, ‘–‘ …etc are not allowed.
  */
-auto Subtract1(int num) {
-    int mask = 1;
+auto Subtract1(unsigned num) {
+    unsigned mask = 1;
     for (; not(num & mask); mask <<= 1) {
         num ^= mask;
     }
 
-    return    num ^ mask;
+    return num ^ mask;
 }
+
+
+auto Subtract1_Plus(const unsigned num) {
+    return ((num << 1) + (~num));
+}
+
+}//namespace
 
 
 SIMPLE_BENCHMARK(Subtract1, 3);
 
-SIMPLE_TEST(Subtract1, TestSAMPLE1, 3, 4);
-SIMPLE_TEST(Subtract1, TestSAMPLE2, 9, 10);
-SIMPLE_TEST(Subtract1, TestSAMPLE3, -2, -1);
+SIMPLE_TEST(Subtract1, TestSAMPLE1, 3u, 4);
+SIMPLE_TEST(Subtract1, TestSAMPLE2, 9u, 10);
+SIMPLE_TEST(Subtract1, TestSAMPLE3, -2u, -1);
+
+
+SIMPLE_BENCHMARK(Subtract1_Plus, 3);
+
+SIMPLE_TEST(Subtract1_Plus, TestSAMPLE1, 3u, 4);
+SIMPLE_TEST(Subtract1_Plus, TestSAMPLE2, 9u, 10);
+SIMPLE_TEST(Subtract1_Plus, TestSAMPLE3, -2u, -1);
