cc5d7546917a38d89f44dd70001e756bc2dec133
Yang Yang <yyang.even@gmail.com>
2019-08-15 12:43:20 -0700

    Text: Add some variations of remove_characters
    Change-Id: I01ff0401a56aa161fd82534bb70ded3d25ca8cd4

--- mathematics/numbers/binary/round_up_to_power_of_2.cpp
+++ mathematics/numbers/binary/round_up_to_power_of_2.cpp
@@ -2,6 +2,9 @@
 
 #include "set_all_bits_after_most_significant_bit.h"
 
+
+namespace {
+
 typedef unsigned InputType;
 
 /** Round up to the next highest power of 2 by float casting
@@ -14,7 +17,7 @@ typedef unsigned InputType;
  *
  * So num=3 -> r=4; num=8 -> r=8
  */
-InputType RoundUpToPowerOf2Float(const InputType num) {
+InputType RoundUpToPowerOf2_Float(const InputType num) {
     static_assert(Bits_Number<decltype(num)>() == 32, "InputType is not 32 bits.");
 
     if (num) {
@@ -45,24 +48,28 @@ InputType RoundUpToPowerOf2(const InputType num) {
     }
 }
 
+}//namespace
+
 
 constexpr auto LOWER = std::numeric_limits<InputType>::min();
 constexpr InputType UPPER = 1u << (Bits_Number<InputType>() - 1u);
 
-SIMPLE_BENCHMARK(RoundUpToPowerOf2Float, UPPER);
 
-SIMPLE_TEST(RoundUpToPowerOf2Float, TestLOWER, 1, LOWER);
-SIMPLE_TEST(RoundUpToPowerOf2Float, TestUPPER, UPPER, UPPER);
-SIMPLE_TEST(RoundUpToPowerOf2Float, TestSAMPLE1, 4, 3);
-SIMPLE_TEST(RoundUpToPowerOf2Float, TestSAMPLE2, 8, 8);
-SIMPLE_TEST(RoundUpToPowerOf2Float, TestSAMPLE3, UPPER, UPPER - 1);
+SIMPLE_BENCHMARK(RoundUpToPowerOf2_Float, UPPER);
+
+SIMPLE_TEST(RoundUpToPowerOf2_Float, TestLOWER, 1u, LOWER);
+SIMPLE_TEST(RoundUpToPowerOf2_Float, TestUPPER, UPPER, UPPER);
+SIMPLE_TEST(RoundUpToPowerOf2_Float, TestSAMPLE1, 4u, 3);
+SIMPLE_TEST(RoundUpToPowerOf2_Float, TestSAMPLE2, 8u, 8);
+SIMPLE_TEST(RoundUpToPowerOf2_Float, TestSAMPLE3, UPPER, UPPER - 1);
+
 
 SIMPLE_BENCHMARK(RoundUpToPowerOf2, UPPER);
 
-SIMPLE_TEST(RoundUpToPowerOf2, TestLOWER, 1, LOWER);
+SIMPLE_TEST(RoundUpToPowerOf2, TestLOWER, 1u, LOWER);
 SIMPLE_TEST(RoundUpToPowerOf2, TestUPPER, UPPER, UPPER);
-SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE1, 4, 3);
-SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE2, 8, 8);
+SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE1, 4u, 3);
+SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE2, 8u, 8);
 SIMPLE_TEST(RoundUpToPowerOf2, TestSAMPLE3, UPPER, UPPER - 1);
 
-MUTUAL_RANDOM_TEST(RoundUpToPowerOf2Float, RoundUpToPowerOf2, LOWER, UPPER);
+MUTUAL_RANDOM_TEST(RoundUpToPowerOf2_Float, RoundUpToPowerOf2, LOWER, UPPER);
--- text/remove_characters.cpp
+++ text/remove_characters.cpp
@@ -15,7 +15,7 @@ namespace {
 auto RemoveSpaces(std::string input) {
     auto output_iter = input.begin();
     for (auto input_iter = input.begin(); input_iter != input.end(); ++input_iter) {
-        if (*input_iter != ' ') {
+        if (not std::isspace(*input_iter)) {
             *output_iter++ = *input_iter;
         }
     }
@@ -41,16 +41,95 @@ auto RemoveSpaces_StringStream(std::string input) {
     return input;
 }
 
+
+template <typename UnaryPredicate>
+auto RemoveCharacters_Partition(std::string input, const UnaryPredicate p) {
+    const auto end = std::stable_partition(input.begin(), input.end(), p);
+    return input.substr(0, std::distance(input.begin(), end));
+}
+
+auto RemoveSpaces_Partition(const std::string &input) {
+    return RemoveCharacters_Partition(input, ToNegationLambda(std::isspace));
+}
+
+
+/**
+ * @reference   Move spaces to front of string in single traversal
+ *              https://www.geeksforgeeks.org/move-spaces-front-string-single-traversal/
+ */
+
+
+/** Remove all occurrences of a character in a string
+ *
+ * @reference   https://www.geeksforgeeks.org/remove-all-occurrences-of-a-character-in-a-string/
+ */
+auto RemoveCharactersByKey_Partition(const std::string &input, const std::string::value_type key) {
+    return RemoveCharacters_Partition(input, [key](const auto c) {
+        return key != c;
+    });
+}
+
+
+/** Removing punctuations from a given string
+ *
+ * @reference   https://www.geeksforgeeks.org/removing-punctuations-given-string/
+ */
+auto RemovePunctuations_Partition(const std::string &input) {
+    return RemoveCharacters_Partition(input, ToNegationLambda(std::ispunct));
+}
+
+
+/** Remove characters from the first string which are present in the second string
+ *
+ * @reference   https://www.geeksforgeeks.org/remove-characters-from-the-first-string-which-are-present-in-the-second-string/
+ */
+auto RemoveCharactersPresentInTheSecond_Partition(const std::string &input,
+        const std::string &mask) {
+    std::unordered_set<std::string::value_type> counter;
+    for (const auto c : mask) {
+        counter.insert(c);
+    }
+
+    return RemoveCharacters_Partition(input, [&counter](const auto c) {
+        return counter.find(c) == counter.cend();
+    });
+}
+
 }//namespace
 
 
-SIMPLE_BENCHMARK(RemoveSpaces, std::string("g  eeks   for ge  eeks  "));
+SIMPLE_BENCHMARK(RemoveSpaces, "g  eeks   for ge  eeks  ");
 
-SIMPLE_TEST(RemoveSpaces, TestSAMPLE1, std::string("geeksforgeeeks"),
-            std::string("g  eeks   for ge  eeks  "));
+SIMPLE_TEST(RemoveSpaces, TestSAMPLE1, "geeksforgeeeks", "g  eeks   for ge  eeks  ");
 
 
-SIMPLE_BENCHMARK(RemoveSpaces_StringStream, std::string("g  eeks   for ge  eeks  "));
+SIMPLE_BENCHMARK(RemoveSpaces_StringStream, "g  eeks   for ge  eeks  ");
 
-SIMPLE_TEST(RemoveSpaces_StringStream, TestSAMPLE1, std::string("geeksforgeeeks"),
-            std::string("g  eeks   for ge  eeks  "));
+SIMPLE_TEST(RemoveSpaces_StringStream, TestSAMPLE1, "geeksforgeeeks", "g  eeks   for ge  eeks  ");
+
+
+SIMPLE_BENCHMARK(RemoveSpaces_Partition, "g  eeks   for ge  eeks  ");
+
+SIMPLE_TEST(RemoveSpaces_Partition, TestSAMPLE1, "geeksforgeeeks", "g  eeks   for ge  eeks  ");
+
+
+SIMPLE_BENCHMARK(RemoveCharactersByKey_Partition, "g  eeks   for ge  eeks  ", ' ');
+
+SIMPLE_TEST(RemoveCharactersByKey_Partition, TestSAMPLE1, "geeksforgeeeks",
+            "g  eeks   for ge  eeks  ", ' ');
+SIMPLE_TEST(RemoveCharactersByKey_Partition, TestSAMPLE2, "gksforgks", "geeksforgeeks", 'e');
+SIMPLE_TEST(RemoveCharactersByKey_Partition, TestSAMPLE3, "eeksforeeks", "geeksforgeeks", 'g');
+
+
+SIMPLE_BENCHMARK(RemovePunctuations_Partition, "%welcome' to @geeksforgeek<s");
+
+SIMPLE_TEST(RemovePunctuations_Partition, TestSAMPLE1, "welcome to geeksforgeeks",
+            "%welcome' to @geeksforgeek<s");
+SIMPLE_TEST(RemovePunctuations_Partition, TestSAMPLE2, "Hello he said and went",
+            "Hello!!!, he said ---and went.");
+
+
+SIMPLE_BENCHMARK(RemoveCharactersPresentInTheSecond_Partition, "geeksforgeeks", "mask");
+
+SIMPLE_TEST(RemoveCharactersPresentInTheSecond_Partition, TestSAMPLE1, "geeforgee",
+            "geeksforgeeks", "mask");
