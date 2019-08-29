7301dace328eda1fd322cd29f983f6606d91374e
Yang Yang <yyang.even@gmail.com>
2019-08-29 11:43:25 -0700

    Binary: Merge stripping_off_lowest_set_bit into clear_bit
    Change-Id: I348fa6d33b55a15101ac58f2afd2a4df1a08b814

--- mathematics/numbers/binary/clear_bit.cpp
+++ mathematics/numbers/binary/clear_bit.cpp
@@ -42,6 +42,8 @@ auto ClearBitsFromMSB(const InputType num, const InputType i) {
 /** Turn off the rightmost set bit
  *
  * @reference   https://www.geeksforgeeks.org/turn-off-the-rightmost-set-bit/
+ * @reference   Bitwise Hacks for Competitive Programming
+ *              https://www.geeksforgeeks.org/bitwise-hacks-for-competitive-programming/
  */
 auto ClearRightmostBit(const InputType num) {
     return num & (num - 1);
