5592a092568300c181f39b2e7b47e14aec594abe
Yang Yang <yyang.even@gmail.com>
2020-03-02 11:45:31 -0800

    BST: Add BST_delete
    Change-Id: If0cec93c8c5907002cd50640b3b1fabd93083f6c

--- data_structure/tree/binary_tree/BST/BST_insert.cpp
+++ data_structure/tree/binary_tree/BST/BST_insert.cpp
@@ -10,6 +10,8 @@ namespace {
 /** Binary Search Tree | Set 1 (Search and Insertion)
  *
  * @reference   https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+ * @reference   Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+ *              Introduction to Algorithms, Third Edition. Chapter 12.3. Exercises 12.3-1.
  */
 auto BSTInsert_Recursive(const BinaryTree::Node::PointerType node,
                          const BinaryTree::Node::ValueType x) {
