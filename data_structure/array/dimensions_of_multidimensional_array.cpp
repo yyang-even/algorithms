bac4e3a18432566bdcc349850426b4defdb06375
Yang Yang <yyang.even@gmail.com>
2020-03-19 15:18:56 -0700

    Array: Add dimensions_of_multidimensional_array
    Change-Id: Id270ad1c701964edcd0fe87a1eb7823980d78591

---
+++ data_structure/array/dimensions_of_multidimensional_array.cpp
@@ -0,0 +1,76 @@
+#include "common_header.h"
+
+
+namespace {
+
+using ArrayType = std::vector<unsigned>;
+
+/** How to print dimensions of multidimensional array in C++
+ *
+ * @reference   https://www.geeksforgeeks.org/print-dimensions-multidimensional-array-c/
+ * @reference   Initialization of a multidimensional arrays in C/C++
+ *              https://www.geeksforgeeks.org/g-fact-44/
+ * @reference   Passing a 2D array to a C++ function
+ *              https://stackoverflow.com/questions/8767166/passing-a-2d-array-to-a-c-function
+ */
+template <typename T, std::size_t row_size>
+void DimensionsOfMultidimensionalArray(const T(&)[row_size], ArrayType &outputs) {
+    outputs.push_back(row_size);
+}
+
+template <typename T, std::size_t row_size, std::size_t column_size>
+void DimensionsOfMultidimensionalArray(const T(&elements)[row_size][column_size],
+                                       ArrayType &outputs) {
+    outputs.push_back(row_size);
+    DimensionsOfMultidimensionalArray(elements[0], outputs);
+}
+
+
+template <typename T>
+std::enable_if_t<std::rank<T>::value == 1>
+DimensionsOfMultidimensionalArray_EnableIf(const T &, ArrayType &outputs) {
+    outputs.push_back(std::extent<T>::value);
+}
+
+template <typename T>
+std::enable_if_t < std::rank<T>::value != 1 >
+DimensionsOfMultidimensionalArray_EnableIf(const T &elements, ArrayType &outputs) {
+    outputs.push_back(std::extent<T>::value);
+    DimensionsOfMultidimensionalArray_EnableIf(elements[0], outputs);
+}
+
+}//namespace
+
+
+#ifdef WANT_TESTS
+#define TestDimensionsOfTwoDimensionalArray(funcName, testName, dimension1, dimension2) namespace { \
+    TEST(DimensionsOfTwoDimensionalArrayTest, testName) {                                           \
+        ArrayType outputs;                                                                          \
+        int elements[dimension1][dimension2] = {};                                                  \
+        funcName(elements, outputs);                                                                \
+                                                                                                    \
+        ArrayType EXPECTED_DIMENSIONS = {dimension1, dimension2};                                   \
+        EXPECT_EQ(EXPECTED_DIMENSIONS, outputs);                                                    \
+    }                                                                                               \
+}
+
+#define TestDimensionsOfThreeDimensionalArray(funcName, testName, dimension1, dimension2, dimension3) namespace {   \
+    TEST(DimensionsOfThreeDimensionalArrayTest, testName) {                                                         \
+        ArrayType outputs;                                                                                          \
+        int elements[dimension1][dimension2][dimension3] = {};                                                      \
+        funcName(elements, outputs);                                                                                \
+                                                                                                                    \
+        ArrayType EXPECTED_DIMENSIONS = {dimension1, dimension2, dimension3};                                       \
+        EXPECT_EQ(EXPECTED_DIMENSIONS, outputs);                                                                    \
+    }                                                                                                               \
+}
+
+
+TestDimensionsOfTwoDimensionalArray(DimensionsOfMultidimensionalArray, TestSAMPLE1, 5, 8);
+TestDimensionsOfThreeDimensionalArray(DimensionsOfMultidimensionalArray, TestSAMPLE2, 5, 8, 3);
+
+
+TestDimensionsOfTwoDimensionalArray(DimensionsOfMultidimensionalArray_EnableIf, TestSAMPLE3, 5, 8);
+TestDimensionsOfThreeDimensionalArray(DimensionsOfMultidimensionalArray_EnableIf, TestSAMPLE4, 
+        5, 8, 3);
+#endif
