dcd6527c59469092ca52dc4dc0094b5fea8cbc51
Yang Yang <yyang.even@gmail.com>
2020-04-08 17:30:08 -0700

    N-aryTree: Add more solutions for n-ary_tree_traversal
    Change-Id: I13e471baa3bcc49341510807cdd7dfb2c8a0cb06

--- data_structure/tree/n-ary_tree/left-child_right-sibling_representation.h
+++ data_structure/tree/n-ary_tree/left-child_right-sibling_representation.h
@@ -27,6 +27,23 @@ struct Node {
     explicit Node(ValueType v) : value(std::move(v)) {}
 };
 
+
+/**
+ * @reference   Creating a tree with Left-Child Right-Sibling Representation
+ *              https://www.geeksforgeeks.org/creating-tree-left-child-right-sibling-representation/
+ */
+auto AddSibling(Node::PointerType node, Node::ValueType v) {
+    for (; node->right_sibling; node = node->right_sibling);
+    return node->right_sibling = std::make_shared<Node>(std::move(v));
+}
+
+auto AddChild(Node &parent, Node::ValueType v) {
+    if (parent.left_child) {
+        return AddSibling(parent.left_child, std::move(v));
+    }
+    return parent.left_child = std::make_shared<Node>(std::move(v));
+}
+
 } //namespace child_sibling_representation
 
 } //namespace n_ary_tree
--- data_structure/tree/n-ary_tree/n-ary_tree.h
+++ data_structure/tree/n-ary_tree/n-ary_tree.h
@@ -2,3 +2,28 @@
 
 #include "array_representation.h"
 #include "left-child_right-sibling_representation.h"
+
+
+/**
+ *            1
+ *     /    /    \    \
+ *    2    3     4     5
+ *   / \         |   / | \
+ *  6   7        8  9 10 11
+ */
+template <typename Node>
+auto MakeTheSampleTree() {
+    const auto root = std::make_shared<Node>(1);
+    auto &node2 = *AddChild(*root, 2);
+    AddChild(*root, 3);
+    auto &node4 = *AddChild(*root, 4);
+    auto &node5 = *AddChild(*root, 5);
+    AddChild(node2, 6);
+    AddChild(node2, 7);
+    AddChild(node4, 8);
+    for (auto i = 9; i < 12; ++i) {
+        AddChild(node5, i);
+    }
+
+    return root;
+}
