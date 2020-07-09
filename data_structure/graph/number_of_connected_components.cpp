c63ec81d4dbffa5e73089467db0f3af7dfbb69c0
Yang Yang <yyang.even@gmail.com>
2020-07-09 13:01:07 -0700

    Graph: Add number_of_connected_components
    Change-Id: I6f270aeb38011aac24bf23b637ec4274861f3b59

---
+++ data_structure/graph/number_of_connected_components.cpp
@@ -0,0 +1,49 @@
+#include "common_header.h"
+
+#include "graph.h"
+#include "depth_first_search.h"
+
+
+namespace {
+
+/** Program to count Number of connected components in an undirected graph
+ *
+ * @reference   https://www.geeksforgeeks.org/program-to-count-number-of-connected-components-in-an-undirected-graph/
+ */
+auto CountNumberOfConnectedComponents(const std::size_t number_vertices,
+                                      const UndirectedEdgeArrayType &edges) {
+    unsigned count = 0;
+    GraphTraverse(number_vertices, edges,
+    [&count](const auto & graph, const auto source, auto & visited_vertices) {
+        AdjacencyListGraph::ArrayType results;
+        DepthFirstSearch_Recursive(graph, source, visited_vertices, results);
+        ++count;
+        return true;
+    });
+
+    return true;
+}
+
+
+/**
+ * @reference   Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+ *              Introduction to Algorithms, Third Edition. Exercises 22.3-12.
+ *
+ * Show that we can use a depth-first search of an undirected graph G to identify the connected
+ * components of G, and that the depth-first forest contains as many trees as G has connected
+ * components. More precisely, show how to modify depth-first search so that it assigns to each
+ * vertex v an integer label v.cc between 1 and k, where k is the number of connected components
+ * of G, such that u.cc = v.cc if and only if u and v are in the same connected component.
+ */
+
+}//namespace
+
+
+const UndirectedEdgeArrayType SAMPLE1 = {{1, 5}, {0, 2}, {2, 4}};
+const UndirectedEdgeArrayType SAMPLE2 = {{1, 0}, {2, 3}, {3, 4}};
+
+
+SIMPLE_BENCHMARK(CountNumberOfConnectedComponents, 6, SAMPLE1);
+
+SIMPLE_TEST(CountNumberOfConnectedComponents, TestSAMPLE1, 3, 6, SAMPLE1);
+SIMPLE_TEST(CountNumberOfConnectedComponents, TestSAMPLE2, 2, 5, SAMPLE2);
