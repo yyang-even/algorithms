394d6ff9581ff00fa28fd0d4b49bcf74a8f818ce
Yang Yang <yyang.even@gmail.com>
2020-10-15 13:37:33 -0700

    Pre-commit: Adjust line length to 88
    Change-Id: I47c1ae943698dba150dcc0713092a828f3d52044

--- ordering/sorted_insert.h
+++ ordering/sorted_insert.h
@@ -1,6 +1,7 @@
 #pragma once
 
-void SortedInsert(const std::vector<int>::iterator begin, const std::vector<int>::iterator last) {
+void SortedInsert(const std::vector<int>::iterator begin,
+                  const std::vector<int>::iterator last) {
     assert(std::is_sorted(begin, last));
     const auto insertion_point = std::upper_bound(begin, last, *last);
     std::rotate(insertion_point, last, std::next(last));
