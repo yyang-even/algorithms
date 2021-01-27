cf1a19ad0ba6ca3d98fbfe3fb64faf98d74db67d
Yang Yang <yyang.even@gmail.com>
2021-01-27 11:05:26 -0800

    Hash: Add check_if_one_string_can_be_formed_from_another
    Change-Id: I6a97e1e82c028bcee3db7f8803f66a0fada57f80

---
+++ hash/check_if_one_string_can_be_formed_from_another.cpp
@@ -0,0 +1,53 @@
+#include "common_header.h"
+
+
+namespace {
+
+/** Check whether second string can be formed from characters of first string
+ *
+ * @reference   https://www.geeksforgeeks.org/check-whether-second-string-can-formed-first-string-using-count-array/
+ */
+auto CanSecondStringBeFormedFromFirst(const std::string &given_str,
+                                      const std::string &candidate) {
+    std::unordered_map<std::string::value_type, int> char_counts;
+    for (const auto c : candidate) {
+        ++char_counts[c];
+    }
+
+    for (const auto c : given_str) {
+        const auto iter = char_counts.find(c);
+        if (iter != char_counts.cend()) {
+            if (--iter->second == 0) {
+                char_counts.erase(iter);
+                if (char_counts.empty()) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+
+/** Check whether second string can be formed from words of first string
+ *
+ * @reference   Section VI. Five Algorithm Approaches.
+ *              Gayle Laakmann McDowell. Cracking the Coding Interview, Fifth Edition.
+ *
+ * A ransom note can be formed by cutting words out of a magazine to form a new sentence.
+ * How would your figure out if a ransom note (represented as a string) can be formed
+ * from a given magazine (string)?
+ */
+
+}//namespace
+
+
+THE_BENCHMARK(CanSecondStringBeFormedFromFirst, "geekforgeeks", "geeks");
+
+SIMPLE_TEST(CanSecondStringBeFormedFromFirst, TestSAMPLE1, true,
+            "geekforgeeks", "geeks");
+SIMPLE_TEST(CanSecondStringBeFormedFromFirst, TestSAMPLE2, false,
+            "geekforgeeks", "and");
+SIMPLE_TEST(CanSecondStringBeFormedFromFirst, TestSAMPLE3, true,
+            "geekforgeeks", "geeeek");
