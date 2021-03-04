ddb563006b0f84e856779e68710b2a292650a438
Yang Yang <yyang.even@gmail.com>
2021-03-04 13:49:11 -0800

    Graphics: Add find_max_points_line
    Change-Id: I62b03c43605c26c1aac0787ef71595f8c69bb794

--- graphics/point.h
+++ graphics/point.h
@@ -6,12 +6,16 @@ struct Point {
     double y;
 };
 
+static inline auto Equal(const double x, const double y) {
+    return std::abs(x - y) < std::numeric_limits<double>::epsilon();
+}
+
 static inline std::ostream &operator <<(std::ostream &out, const Point &p) {
     return out << "(" << p.x << ", " << p.y << ")";
 }
 
 static inline auto operator==(const Point &lhs, const Point &rhs) {
-    return lhs.x == rhs.x and lhs.y == rhs.y;
+    return Equal(lhs.x, rhs.x) and Equal(lhs.y, rhs.y);
 }
 
 
@@ -22,7 +26,7 @@ static inline auto operator==(const Point &lhs, const Point &rhs) {
  * Given two co-ordinates, find the slope of a straight line.
  */
 static inline auto Slope(const Point &a_point, const Point &another_point) {
-    assert(a_point.x != another_point.x);
+    assert(not Equal(a_point.x, another_point.x));
 
     return (another_point.y - a_point.y) / (another_point.x - a_point.x);
 }
