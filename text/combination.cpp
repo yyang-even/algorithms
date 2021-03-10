eba31a2ede4f99bf45ab2bb5e85a594c1406157d
Yang Yang <yyang.even@gmail.com>
2021-03-10 11:33:15 -0800

    Text: Add more solutions for combination
    Change-Id: Ib65e311d5a58cfc960c0dc441cc1518a01bb4142

--- text/combination.cpp
+++ text/combination.cpp
@@ -8,11 +8,14 @@ using ArrayType = std::unordered_multiset<std::string>;
 /** Print all possible combinations of r elements in a given array of size n
  *
  * @reference   https://www.geeksforgeeks.org/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
+ * @reference   Print all subsets of given size of a set
+ *              https://www.geeksforgeeks.org/print-subsets-given-size-set/
  *
  * @reference   Make all combinations of size k
  *              https://www.geeksforgeeks.org/make-combinations-size-k/
  *
- * Given two numbers n and k and you have to find all possible combination of k numbers from 1...n.
+ * Given two numbers n and k and you have to find all possible combination of k numbers
+ * from 1...n.
  */
 void CombinationsOfLength_Start(const std::string &elements,
                                 const std::string::size_type length,
@@ -76,23 +79,82 @@ auto CombinationsOfLength_Include(const std::string &elements,
  *
  * @reference   John Mongan, Eric Giguere, Noah Kindler.
  *              Programming Interviews Exposed, Third Edition. Chapter 7.
+ * @reference   Backtracking to find all subsets
+ *              https://www.geeksforgeeks.org/backtracking-to-find-all-subsets/
+ * @reference   Gayle Laakmann McDowell. Cracking the Coding Interview, Fifth Edition.
+ *              Questions 9.4.
  */
-void AllCombinations(const std::string &elements,
-                     const std::string::size_type start,
-                     std::string &one_combination,
-                     ArrayType &results) {
+void AllCombinations_Recursive(const std::string &elements,
+                               const std::string::size_type start,
+                               std::string &one_combination,
+                               ArrayType &results) {
     for (auto i = start; i < elements.size(); ++i) {
         one_combination.push_back(elements[i]);
         results.insert(one_combination);
-        AllCombinations(elements, i + 1, one_combination, results);
+        AllCombinations_Recursive(elements, i + 1, one_combination, results);
         one_combination.pop_back();
     }
 }
 
-auto AllCombinations(const std::string &elements) {
+auto AllCombinations_Recursive(const std::string &elements) {
     std::string one_combination;
     ArrayType results;
-    AllCombinations(elements, 0, one_combination, results);
+    AllCombinations_Recursive(elements, 0, one_combination, results);
+
+    return results;
+}
+
+
+/**
+ * @reference   Power Set
+ *              https://www.geeksforgeeks.org/power-set/
+ * @reference   Find all distinct subsets of a given set using BitMasking Approach
+ *              https://www.geeksforgeeks.org/find-distinct-subsets-given-set/
+ * @reference   Printing all subsets of {1,2,3,...n} without using array or loop
+ *              https://www.geeksforgeeks.org/printing-all-subsets-of-123-n-without-using-array-or-loop/
+ */
+auto AllCombinations_BitMask(const std::string &elements) {
+    assert(elements.size() < BitsNumber<unsigned long long>);
+
+    const auto power_set_size = 1ull << elements.size();
+
+    ArrayType results;
+    for (auto mask = 1ull; mask < power_set_size; ++mask) {
+        std::string a_subset;
+        for (std::size_t i = 0; i < elements.size(); ++i) {
+            if (mask & (1ull << i)) {
+                a_subset.push_back(elements[i]);
+            }
+        }
+        results.insert(a_subset);
+    }
+
+    return results;
+}
+
+
+/**
+ * @reference   Recursive program to generate power set
+ *              https://www.geeksforgeeks.org/recursive-program-to-generate-power-set/
+ */
+void AllCombinations_Recursive_Copy(const std::string &elements,
+                                    ArrayType &results,
+                                    const std::string::size_type index = 0,
+                                    const std::string subset = "") {
+    if (index == elements.size()) {
+        if (not subset.empty()) {
+            results.insert(subset);
+        }
+        return;
+    }
+
+    AllCombinations_Recursive_Copy(elements, results, index + 1, subset + elements[index]);
+    AllCombinations_Recursive_Copy(elements, results, index + 1, subset);
+}
+
+auto AllCombinations_Recursive_Copy(const std::string &elements) {
+    ArrayType results;
+    AllCombinations_Recursive_Copy(elements, results);
 
     return results;
 }
@@ -121,8 +183,22 @@ SIMPLE_TEST(CombinationsOfLength_Include, TestSAMPLE4, EXPECTED4, "wxyz", 2);
 SIMPLE_TEST(CombinationsOfLength_Include, TestSAMPLE5, EXPECTED5, "12345", 3);
 
 
-THE_BENCHMARK(AllCombinations, "abcd");
+THE_BENCHMARK(AllCombinations_Recursive, "abcd");
 
-SIMPLE_TEST(AllCombinations, TestSAMPLE1, EXPECTED1, "a");
-SIMPLE_TEST(AllCombinations, TestSAMPLE2, EXPECTED2, "ab");
-SIMPLE_TEST(AllCombinations, TestSAMPLE3, EXPECTED3, "wxyz");
+SIMPLE_TEST(AllCombinations_Recursive, TestSAMPLE1, EXPECTED1, "a");
+SIMPLE_TEST(AllCombinations_Recursive, TestSAMPLE2, EXPECTED2, "ab");
+SIMPLE_TEST(AllCombinations_Recursive, TestSAMPLE3, EXPECTED3, "wxyz");
+
+
+THE_BENCHMARK(AllCombinations_BitMask, "abcd");
+
+SIMPLE_TEST(AllCombinations_BitMask, TestSAMPLE1, EXPECTED1, "a");
+SIMPLE_TEST(AllCombinations_BitMask, TestSAMPLE2, EXPECTED2, "ab");
+SIMPLE_TEST(AllCombinations_BitMask, TestSAMPLE3, EXPECTED3, "wxyz");
+
+
+THE_BENCHMARK(AllCombinations_Recursive_Copy, "abcd");
+
+SIMPLE_TEST(AllCombinations_Recursive_Copy, TestSAMPLE1, EXPECTED1, "a");
+SIMPLE_TEST(AllCombinations_Recursive_Copy, TestSAMPLE2, EXPECTED2, "ab");
+SIMPLE_TEST(AllCombinations_Recursive_Copy, TestSAMPLE3, EXPECTED3, "wxyz");
