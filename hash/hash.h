1a1c1b3899b5c525063f82da59ca61de6996d01f
Yang Yang <yyang.even@gmail.com>
2021-06-17 17:32:12 -0700

    DP: Add matchsticks_to_square
    Change-Id: I9331187bd0b9142d1744737069f8399147857571

--- hash/hash.h
+++ hash/hash.h
@@ -20,3 +20,14 @@ ToUnorderedSet(Container container) {
     using SetType = std::unordered_set<typename Container::value_type>;
     return ContainerCast<SetType>(std::move(container));
 }
+
+
+struct PairHash {
+    template <class T1, class T2>
+    auto operator()(const std::pair<T1, T2> &p) const {
+        const auto h1 = std::hash<T1> {}(p.first);
+        const auto h2 = std::hash<T2> {}(p.second);
+
+        return h1 ^ h2;
+    }
+};
