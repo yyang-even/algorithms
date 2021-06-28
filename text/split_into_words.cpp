e3d12c883bed6a9f1351205d84db2b505693c9e8
Yang Yang <yyang.even@gmail.com>
2021-06-28 15:36:24 -0700

    Text: Add recursively_remove_adjacent_duplicates
    Change-Id: I39834a9e09288e89426360a9603ac9dcb434f5a0

--- text/split_into_words.cpp
+++ text/split_into_words.cpp
@@ -58,6 +58,16 @@ auto SplitIntoWords_StringStream(const std::string &sentence) {
     return outputs;
 }
 
+
+/**
+ * @reference   Length of Last Word
+ *              https://leetcode.com/problems/length-of-last-word/
+ *
+ * Given a string s consists of some words separated by spaces, return the length of the
+ * last word in the string. If the last word does not exist, return 0. A word is a
+ * maximal substring consisting of non-space characters only.
+ */
+
 }//namespace
 
 
