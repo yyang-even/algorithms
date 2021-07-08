a17ab6c44b24bd911b4ce45add2d56573c428c82
Yang Yang <yyang.even@gmail.com>
2021-07-08 14:40:50 -0700

    LinkedList: Add intersection_point
    Change-Id: I018132d74226a62b3eea3c8c758a7c6505d20fa2

---
+++ data_structure/linked_list/intersection_point.cpp
@@ -0,0 +1,173 @@
+#include "common_header.h"
+
+#include "singly_list/singly_linked_list.h"
+
+
+namespace {
+
+using ArrayType = std::vector<int>;
+
+/** Intersection of Two Linked Lists
+ *
+ * @reference   https://leetcode.com/problems/intersection-of-two-linked-lists/
+ *
+ * Given the heads of two singly linked-lists headA and headB, return the node at which
+ * the two lists intersect. If the two linked lists have no intersection at all, return
+ * null. It is guaranteed that there are no cycles anywhere in the entire linked
+ * structure. Note that the linked lists must retain their original structure after the
+ * function returns. Follow up: Could you write a solution that runs in O(n) time and
+ * use only O(1) memory?
+ *  1 <= Node.val <= 105
+ *
+ * @reference   Write a function to get the intersection point of two Linked Lists
+ *              https://www.geeksforgeeks.org/write-a-function-to-get-the-intersection-point-of-two-linked-lists/
+ */
+auto IntersectionPoint_Length(const SinglyLinkedList::Node::PointerType one_head,
+                              const SinglyLinkedList::Node::PointerType another_head) {
+    const int one_size = CountSize_Iterative(one_head);
+    const int another_size = CountSize_Iterative(another_head);
+
+    const auto diff = std::abs(one_size - another_size);
+    auto shorter = one_head;
+    auto longer = another_head;
+    if (one_size > another_size) {
+        std::swap(shorter, longer);
+    }
+
+    for (int i = 0; i < diff; ++i) {
+        longer = longer->next;
+    }
+
+    while (longer and shorter) {
+        if (longer == shorter) {
+            return longer;
+        }
+
+        longer = longer->next;
+        shorter = shorter->next;
+    }
+
+    return SinglyLinkedList::Node::PointerType{};
+}
+
+
+/**
+ * @reference   Write a function to get the intersection point of two Linked Lists | Set 2
+ *              https://www.geeksforgeeks.org/write-a-function-to-get-the-intersection-point-of-two-linked-lists-set-2/
+ * @reference   Find intersection point of two Linked Lists without finding the length
+ *              https://www.geeksforgeeks.org/find-intersection-point-of-two-linked-lists-without-finding-the-length/
+ */
+inline auto
+IntersectionPoint_TwoPointers(const SinglyLinkedList::Node::PointerType one_head,
+                              const SinglyLinkedList::Node::PointerType another_head) {
+    auto one_ptr = one_head;
+    auto another_ptr = another_head;
+
+    while (one_ptr != another_ptr) {
+        if (not one_ptr) {
+            one_ptr = another_head;
+        } else {
+            one_ptr = one_ptr->next;
+        }
+
+        if (not another_ptr) {
+            another_ptr = one_head;
+        } else {
+            another_ptr = another_ptr->next;
+        }
+    }
+
+    return one_ptr;
+}
+
+
+/**
+ * @reference   Intersection point of two Linked Lists | Set 3
+ *              https://www.geeksforgeeks.org/intersection-point-of-two-linked-lists-set-3/
+ *
+ * Given two linked lists of size N and M consisting of positive value nodes, having a
+ * common intersection point, the task is to find the intersection point of the two
+ * linked lists where they merge.
+ */
+inline auto
+IntersectionPoint_Flag(SinglyLinkedList::Node::PointerType one_head,
+                       SinglyLinkedList::Node::PointerType another_head) {
+    for (; one_head; one_head = one_head->next) {
+        one_head->value *= -1;
+    }
+
+    for (; another_head; another_head = another_head->next) {
+        if (another_head->value < 0) {
+            another_head->value *= -1;
+            return another_head;
+        }
+    }
+
+    return SinglyLinkedList::Node::PointerType{};
+}
+
+
+template <typename Function>
+constexpr auto
+testIntersectionPoint(const ArrayType &one, const ArrayType &another,
+                      const int intersection_rindex, const Function intersection_point) {
+    const auto one_head = SinglyLinkedList{one}.GetHead();
+    const auto another_head = SinglyLinkedList{another}.GetHead();
+
+    if (intersection_rindex == -1) {
+        return nullptr == intersection_point(one_head, another_head);
+    }
+
+    const auto expected = *(one.crbegin() + intersection_rindex);
+    assert(another.size() >= one.size());
+    const int diff = another.size() - one.size();
+    auto one_iter = one_head;
+    auto another_iter = another_head;
+    for (int i = 0; i < diff; ++i) {
+        another_iter = another_iter->next;
+    }
+
+    for (std::size_t i = 1; i < one.size() - intersection_rindex - 1; ++i) {
+        one_iter = one_iter->next;
+        another_iter = another_iter->next;
+    }
+
+    another_iter->next = one_iter->next;
+
+    return expected == intersection_point(one_head, another_head)->value;
+}
+
+}//namespace
+
+
+const ArrayType SAMPLE1_L = {4, 1, 8, 4, 5};
+const ArrayType SAMPLE1_R = {5, 6, 1, 8, 4, 5};
+
+const ArrayType SAMPLE2_L = {3, 2, 4};
+const ArrayType SAMPLE2_R = {1, 9, 1, 2, 4};
+
+
+THE_BENCHMARK(testIntersectionPoint, SAMPLE1_L, SAMPLE1_R, 2, IntersectionPoint_Length);
+
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE1L, true,
+            SAMPLE1_L, SAMPLE1_R, 2, IntersectionPoint_Length);
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE2L, true,
+            SAMPLE2_L, SAMPLE2_R, 1, IntersectionPoint_Length);
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE3L, true,
+            SAMPLE2_L, SAMPLE2_R, 1, IntersectionPoint_Length);
+
+
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE1T, true,
+            SAMPLE1_L, SAMPLE1_R, 2, IntersectionPoint_TwoPointers);
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE2T, true,
+            SAMPLE2_L, SAMPLE2_R, 1, IntersectionPoint_TwoPointers);
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE3T, true,
+            SAMPLE2_L, SAMPLE2_R, 1, IntersectionPoint_TwoPointers);
+
+
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE1F, true,
+            SAMPLE1_L, SAMPLE1_R, 2, IntersectionPoint_Flag);
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE2F, true,
+            SAMPLE2_L, SAMPLE2_R, 1, IntersectionPoint_Flag);
+SIMPLE_TEST(testIntersectionPoint, TestSAMPLE3F, true,
+            SAMPLE2_L, SAMPLE2_R, 1, IntersectionPoint_Flag);
--- dynamic_programming/longest_common_substring.cpp
+++ dynamic_programming/longest_common_substring.cpp
@@ -9,18 +9,26 @@ namespace {
  *              https://www.geeksforgeeks.org/longest-common-substring-dp-29/
  * @reference   Longest common substring in binary representation of two numbers
  *              https://www.geeksforgeeks.org/longest-common-substring-binary-representation-two-numbers/
+ *
+ * @reference   Longest common subarray in the given two arrays
+ *              https://www.geeksforgeeks.org/longest-common-subarray-in-the-given-two-arrays/
+ * @reference   Maximum Length of Repeated Subarray
+ *              https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+ *
+ * Given two integer arrays nums1 and nums2, return the maximum length of a subarray
+ * that appears in both arrays.
  */
 constexpr auto
 LongestCommonSubstring(const std::string_view X, const std::string_view Y) {
-    std::size_t LCSs[X.size() + 1][Y.size() + 1] = {};
+    std::size_t longest_common_suffix[X.size() + 1][Y.size() + 1] = {};
     std::size_t max_length = 0;
 
     for (std::size_t i = 1; i <= X.size(); ++i) {
         for (std::size_t j = 1; j <= Y.size(); ++j) {
             if (X[i - 1] == Y[j - 1]) {
-                LCSs[i][j] = LCSs[i - 1][j - 1] + 1;
-                if (max_length < LCSs[i][j]) {
-                    max_length = LCSs[i][j];
+                longest_common_suffix[i][j] = longest_common_suffix[i - 1][j - 1] + 1;
+                if (max_length < longest_common_suffix[i][j]) {
+                    max_length = longest_common_suffix[i][j];
                 }
             }
         }
@@ -40,7 +48,7 @@ constexpr auto
 LongestCommonSubstring_SpaceOptimized(const std::string_view X,
                                       const std::string_view Y) {
     auto row_size = 2;  //work around a GCC bug.
-    std::size_t LCSs[row_size][Y.size() + 1] = {};
+    std::size_t longest_common_suffix[row_size][Y.size() + 1] = {};
     std::size_t max_length = 0;
     std::size_t the_end = 0;
 
@@ -48,18 +56,44 @@ LongestCommonSubstring_SpaceOptimized(const std::string_view X,
         const auto current_row = i % 2;
         for (std::size_t j = 1; j <= Y.size(); ++j) {
             if (X[i - 1] == Y[j - 1]) {
-                LCSs[current_row][j] = LCSs[1 - current_row][j - 1] + 1;
-                if (max_length < LCSs[current_row][j]) {
-                    max_length = LCSs[current_row][j];
+                longest_common_suffix[current_row][j] =
+                    longest_common_suffix[1 - current_row][j - 1] + 1;
+                if (max_length < longest_common_suffix[current_row][j]) {
+                    max_length = longest_common_suffix[current_row][j];
                     the_end = i - 1;
                 }
             } else {
-                LCSs[current_row][j] = 0;
+                longest_common_suffix[current_row][j] = 0;
             }
         }
     }
 
     return X.substr(the_end - max_length + 1, max_length);
+}
+
+
+constexpr auto
+LongestCommonSubstring_SlidingWindow(const std::string_view X,
+                                     const std::string_view Y) {
+    std::size_t result = 0;
+    const long x_size = X.size();
+    const long y_size = Y.size();
+    for (long i = 0; i < x_size + y_size - 1; ++i) {
+        auto x_index = std::max(0l, x_size - 1 - i);
+        auto y_index = std::max(0l, i - (x_size - 1));
+        std::size_t common_substring = 0;
+        for (; x_index < x_size and y_index < y_size; ++x_index, ++y_index) {
+            if (X[x_index] == Y[y_index]) {
+                ++common_substring;
+            } else {
+                common_substring = 0;
+            }
+
+            result = std::max(result, common_substring);
+        }
+    }
+
+    return result;
 }
 
 
@@ -101,4 +135,14 @@ SIMPLE_TEST(LongestCommonSubstring_SpaceOptimized, TestSAMPLE1, EXPECTED1,
 SIMPLE_TEST(LongestCommonSubstring_SpaceOptimized, TestSAMPLE2, EXPECTED2,
             SAMPLE2_X, SAMPLE2_Y);
 SIMPLE_TEST(LongestCommonSubstring_SpaceOptimized, TestSAMPLE3, EXPECTED3,
+            SAMPLE3_X, SAMPLE3_Y);
+
+
+THE_BENCHMARK(LongestCommonSubstring_SlidingWindow, SAMPLE1_X, SAMPLE1_Y);
+
+SIMPLE_TEST(LongestCommonSubstring_SlidingWindow, TestSAMPLE1, EXPECTED1.size(),
+            SAMPLE1_X, SAMPLE1_Y);
+SIMPLE_TEST(LongestCommonSubstring_SlidingWindow, TestSAMPLE2, EXPECTED2.size(),
+            SAMPLE2_X, SAMPLE2_Y);
+SIMPLE_TEST(LongestCommonSubstring_SlidingWindow, TestSAMPLE3, EXPECTED3.size(),
             SAMPLE3_X, SAMPLE3_Y);
