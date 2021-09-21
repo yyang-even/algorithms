7e775e0c2861281620abcf89aeb8a9a04b159202
Yang Yang <yyang.even@gmail.com>
2021-09-21 11:51:05 -0700

    DP: Add distinct_subsequences and target_sum
    Change-Id: I28838d7d74e3feb4dfd09c2de5a25bac5a58f9bc

--- data_structure/heap/binary_heap.cpp
+++ data_structure/heap/binary_heap.cpp
@@ -21,6 +21,22 @@ auto testBinaryHeap(const MinHeap<int>::ArrayType &array) {
     return sorted_array;
 }
 
+
+/**
+ * @reference   Last Stone Weight
+ *              https://leetcode.com/problems/last-stone-weight/
+ *
+ * You are given an array of integers stones where stones[i] is the weight of the ith
+ * stone. We are playing a game with the stones. On each turn, we choose the heaviest
+ * two stones and smash them together. Suppose the heaviest two stones have weights x
+ * and y with x <= y. The result of this smash is:
+ *  If x == y, both stones are destroyed, and
+ *  If x != y, the stone of weight x is destroyed, and the stone of weight y has new
+ *      weight y - x.
+ * At the end of the game, there is at most one stone left. Return the smallest
+ * possible weight of the left stone. If there are no stones left, return 0.
+ */
+
 }//namespace
 
 
