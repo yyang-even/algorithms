654f9f84fe2b618cfbf2f63aede644f621b7b0b9
Yang Yang <yyang.even@gmail.com>
2021-10-13 15:09:44 -0700

    Greedy: Add jump_game
    Change-Id: I9bed61fa3321ec0c85235706938c6dc356dde3a6

--- mathematics/numbers/binary/find_most_significant_bit.cpp
+++ mathematics/numbers/binary/find_most_significant_bit.cpp
@@ -1,32 +1,9 @@
 #include "common_header.h"
 
-#include "set_all_bits_after_most_significant_bit.h"
+#include "find_most_significant_bit.h"
 
 
 namespace {
-
-/** Find most significant set bit of a number
- *
- * @reference   https://www.geeksforgeeks.org/find-significant-set-bit-number/
- * @reference   Highest power of 2 less than or equal to given number
- *              https://www.geeksforgeeks.org/highest-power-2-less-equal-given-number/
- *
- * Some people are standing in a queue. A selection process follows a rule where people
- * standing on even positions are selected. Of the selected people a queue is formed and
- * again out of these only people on even position are selected. This continues until we
- * are left with one person. Find out the position of that person in the original queue.
- * Print the position(original queue) of that person who is left.
- *
- * @reference   Highest power of 2 less than or equal to given Integer
- *              https://www.geeksforgeeks.org/highest-power-of-2-less-than-or-equal-to-given-integer/
- *
- * Given a number, find the most significant bit number which is set bit and which is in
- * power of two.
- */
-inline constexpr unsigned FindMSB(const unsigned num) {
-    return (SetAllBitsAfterMSB(num) + 1ull) >> 1;
-}
-
 
 inline constexpr unsigned FindMSB_Float(const unsigned num) {
     return std::pow(2, static_cast<unsigned>(std::log2(num)));
