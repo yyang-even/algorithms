269d5a036c4cc2d77e3ea66fd39a809b0b8569d2
Yang Yang <yyang.even@gmail.com>
2022-03-16 15:14:56 -0700

    Stack: Add validate_stack_sequences
    Change-Id: Ie8f776751da815f7e8d3c7b7959f48045cbebb76

--- hash/count_frequencies_of_all_elements.cpp
+++ hash/count_frequencies_of_all_elements.cpp
@@ -206,6 +206,35 @@ auto SortAndCount_BucketSort(const ArrayType &values) {
  * words[i] consists of lowercase English letters.
  */
 
+
+/**
+ * @reference   Check if All Characters Have Equal Number of Occurrences
+ *              https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/
+ *
+ * Given a string s, return true if s is a good string, or false otherwise.
+ * A string s is good if all the characters that appear in s have the same number of
+ * occurrences (i.e., the same frequency).
+ */
+
+
+/**
+ * @reference   Rings and Rods
+ *              https://leetcode.com/problems/rings-and-rods/
+ *
+ * There are n rings and each ring is either red, green, or blue. The rings are distributed
+ * across ten rods labeled from 0 to 9.
+ * You are given a string rings of length 2n that describes the n rings that are placed onto
+ * the rods. Every two characters in rings forms a color-position pair that is used to
+ * describe each ring where:
+ *  The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
+ *  The second character of the ith pair denotes the rod that the ith ring is placed on
+ *      ('0' to '9').
+ * For example, "R3G2B1" describes n == 3 rings: a red ring placed onto the rod labeled 3,
+ * a green ring placed onto the rod labeled 2, and a blue ring placed onto the rod labeled
+ * 1.
+ * Return the number of rods that have all three colors of rings on them.
+ */
+
 }//namespace
 
 
