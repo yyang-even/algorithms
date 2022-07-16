63ef1c86bd5d51f6560123d038ba0bdcbadae8aa
Yang Yang <yyang.even@gmail.com>
2022-07-15 21:42:47 -0700

    Binary: Add a variant of longest_consecutive_1s
    Change-Id: I0f88ca9dfc0ff5839ffb44e5bd2920b8bd018817

--- data_structure/tree/binary_tree/two_order_to_binary_tree.cpp
+++ data_structure/tree/binary_tree/two_order_to_binary_tree.cpp
@@ -10,24 +10,25 @@ namespace {
  *
  * @reference   https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
  *
- * Given two integer arrays preorder and inorder where preorder is the preorder traversal
- * of a binary tree and inorder is the inorder traversal of the same tree, construct and
- * return the binary tree.
+ * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a
+ * binary tree and inorder is the inorder traversal of the same tree, construct and return the
+ * binary tree.
  * preorder and inorder consist of unique values.
  */
 auto ConstructBinaryTree_PreAndIn(const BinaryTree::ArrayType &preorder,
-                                  std::size_t &pre_i, const std::unordered_map<int, int> &inorder_map,
-                                  const int left, const int right) {
+                                  std::size_t &pre_i,
+                                  const std::unordered_map<int, int> &inorder_map,
+                                  const int left,
+                                  const int right) {
     if (left > right)
-        return BinaryTree::Node::PointerType{};
+        return BinaryTree::Node::PointerType {};
 
     const auto root_value = preorder[pre_i++];
     const auto root = std::make_shared<BinaryTree::Node>(root_value);
 
     const auto mid = inorder_map.at(root_value);
     root->left = ConstructBinaryTree_PreAndIn(preorder, pre_i, inorder_map, left, mid - 1);
-    root->right =
-        ConstructBinaryTree_PreAndIn(preorder, pre_i, inorder_map, mid + 1, right);
+    root->right = ConstructBinaryTree_PreAndIn(preorder, pre_i, inorder_map, mid + 1, right);
 
     return root;
 }
@@ -50,25 +51,25 @@ auto ConstructBinaryTree_PreAndIn(const BinaryTree::ArrayType &preorder,
  * @reference   Construct Binary Tree from Inorder and Postorder Traversal
  *              https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
  *
- * Given two integer arrays inorder and postorder where inorder is the inorder traversal
- * of a binary tree and postorder is the postorder traversal of the same tree, construct
- * and return the binary tree.
+ * Given two integer arrays inorder and postorder where inorder is the inorder traversal of a
+ * binary tree and postorder is the postorder traversal of the same tree, construct and return
+ * the binary tree.
  * inorder and postorder consist of unique values.
  */
 auto ConstructBinaryTree_InAndPost(const BinaryTree::ArrayType &postorder,
-                                   int &post_i, const std::unordered_map<int, int> &inorder_map,
-                                   const int left, const int right) {
+                                   int &post_i,
+                                   const std::unordered_map<int, int> &inorder_map,
+                                   const int left,
+                                   const int right) {
     if (left > right)
-        return BinaryTree::Node::PointerType{};
+        return BinaryTree::Node::PointerType {};
 
     const auto root_value = postorder[post_i--];
     const auto root = std::make_shared<BinaryTree::Node>(root_value);
 
     const auto mid = inorder_map.at(root_value);
-    root->right =
-        ConstructBinaryTree_InAndPost(postorder, post_i, inorder_map, mid + 1, right);
-    root->left = ConstructBinaryTree_InAndPost(postorder, post_i, inorder_map, left,
-                                               mid - 1);
+    root->right = ConstructBinaryTree_InAndPost(postorder, post_i, inorder_map, mid + 1, right);
+    root->left = ConstructBinaryTree_InAndPost(postorder, post_i, inorder_map, left, mid - 1);
 
     return root;
 }
@@ -83,11 +84,10 @@ auto ConstructBinaryTree_InAndPost(const BinaryTree::ArrayType &inorder,
     }
 
     int post_i = postorder.size() - 1;
-    return ConstructBinaryTree_InAndPost(postorder, post_i, index_map, 0,
-                                         inorder.size() - 1);
+    return ConstructBinaryTree_InAndPost(postorder, post_i, index_map, 0, inorder.size() - 1);
 }
 
-}//namespace
+} //namespace
 
 
 const auto SAMPLE1 = MakeTheSampleCompleteTree().GetRoot();
@@ -98,11 +98,17 @@ const BinaryTree::ArrayType SAMPLE1POST = {4, 5, 2, 3, 1};
 
 THE_BENCHMARK(ConstructBinaryTree_PreAndIn, SAMPLE1PRE, SAMPLE1IN);
 
-SIMPLE_TEST(areIdenticalTrees, TestSAMPLE1, true,
-            SAMPLE1, ConstructBinaryTree_PreAndIn(SAMPLE1PRE, SAMPLE1IN));
+SIMPLE_TEST(areIdenticalTrees,
+            TestSAMPLE1,
+            true,
+            SAMPLE1,
+            ConstructBinaryTree_PreAndIn(SAMPLE1PRE, SAMPLE1IN));
 
 
 THE_BENCHMARK(ConstructBinaryTree_InAndPost, SAMPLE1IN, SAMPLE1POST);
 
-SIMPLE_TEST(areIdenticalTrees, TestSAMPLE2, true,
-            SAMPLE1, ConstructBinaryTree_InAndPost(SAMPLE1IN, SAMPLE1POST));
+SIMPLE_TEST(areIdenticalTrees,
+            TestSAMPLE2,
+            true,
+            SAMPLE1,
+            ConstructBinaryTree_InAndPost(SAMPLE1IN, SAMPLE1POST));
