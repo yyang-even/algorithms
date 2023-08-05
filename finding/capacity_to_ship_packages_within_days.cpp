52b8ae01ac388c6e06fac05a90af32b78cae0bfc
Yang Yang <yyang.even@gmail.com>
2023-08-04 22:48:48 -0700

    Greedy: Add find_max_number_of_marked_indices
    Change-Id: I420436c1a4eda16a3c12019d669488e808758a99

--- finding/capacity_to_ship_packages_within_days.cpp
+++ finding/capacity_to_ship_packages_within_days.cpp
@@ -85,6 +85,55 @@ auto MinTimeToCompleteTrips(const ArrayType &time, const int total_trips) {
     return left;
 }
 
+
+/**
+ * @reference   Minimum Speed to Arrive on Time
+ *              https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+ *
+ * You are given a floating-point number hour, representing the amount of time you have to reach the
+ * office. To commute to the office, you must take n trains in sequential order. You are also given an
+ * integer array dist of length n, where dist[i] describes the distance (in kilometers) of the ith train
+ * ride.
+ * Each train can only depart at an integer hour, so you may need to wait in between each train ride.
+ *  For example, if the 1st train ride takes 1.5 hours, you must wait for an additional 0.5 hours before
+ *  you can depart on the 2nd train ride at the 2 hour mark.
+ * Return the minimum positive integer speed (in kilometers per hour) that all the trains must travel at
+ * for you to reach the office on time, or -1 if it is impossible to be on time.
+ * Tests are generated such that the answer will not exceed 107 and hour will have at most two digits
+ * after the decimal point.
+ */
+auto totalTime(const ArrayType &dist, const int speed) {
+    int result = 0;
+    for (std::size_t i = 0; i < dist.size() - 1; ++i) {
+        const auto d = dist[i];
+        result += d / speed + (d % speed != 0);
+    }
+
+    return result + dist.back() / static_cast<double>(speed);
+}
+
+auto MinSpeed(const ArrayType &dist, const double hour) {
+    if (dist.size() - 1 >= hour) {
+        return -1;
+    }
+
+    int left = 1;
+    int right = 1e7;
+
+    while (left < right) {
+        const auto mid = (left + right) / 2;
+        const auto time = totalTime(dist, mid);
+
+        if (time > hour) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return right;
+}
+
 } //namespace
 
 
@@ -108,3 +157,15 @@ THE_BENCHMARK(MinTimeToCompleteTrips, SAMPLE1T, 5);
 
 SIMPLE_TEST(MinTimeToCompleteTrips, TestSAMPLE1, 3, SAMPLE1T, 5);
 SIMPLE_TEST(MinTimeToCompleteTrips, TestSAMPLE2, 2, SAMPLE2T, 1);
+
+
+const ArrayType SAMPLE1D = {1, 3, 2};
+const ArrayType SAMPLE2D = {1, 1, 100000};
+
+
+THE_BENCHMARK(MinSpeed, SAMPLE1D, 6);
+
+SIMPLE_TEST(MinSpeed, TestSAMPLE1, 1, SAMPLE1D, 6);
+SIMPLE_TEST(MinSpeed, TestSAMPLE2, 3, SAMPLE1D, 2.7);
+SIMPLE_TEST(MinSpeed, TestSAMPLE3, -1, SAMPLE1D, 1.9);
+SIMPLE_TEST(MinSpeed, TestSAMPLE4, 10000000, SAMPLE2D, 2.01);
